b470f5be69836c7a1db052b4af1b97218445bad6
Ondra Voves <o.voves@gmail.com>
2015-08-11 22:24:42 +0200

    Squashed 'engine/3rdparty/premake/modules/codelite/' content from commit 287ffc6
    git-subtree-dir: engine/3rdparty/premake/modules/codelite
    git-subtree-split: 287ffc60d66ec36f9120061ddcc9ff44a6950c6f

---
+++ codelite.lua
@@ -0,0 +1,74 @@
+--
+-- Name:        codelite/codelite.lua
+-- Purpose:     Define the CodeLite action(s).
+-- Author:      Ryan Pusztai
+-- Modified by: Andrea Zanellato
+--              Andrew Gough
+--              Manu Evans
+-- Created:     2013/05/06
+-- Copyright:   (c) 2008-2015 Jason Perkins and the Premake project
+--
+
+	local p = premake
+
+	p.modules.codelite = {}
+	p.modules.codelite._VERSION = p._VERSION
+
+	local codelite = p.modules.codelite
+	local project = p.project
+
+
+	function codelite.cfgname(cfg)
+		local cfgname = cfg.buildcfg
+		if codelite.solution.multiplePlatforms then
+			cfgname = string.format("%s|%s", cfg.platform, cfg.buildcfg)
+		end
+		return cfgname
+	end
+
+	function codelite.esc(value)
+		return value
+	end
+
+	function codelite.generateSolution(sln)
+		p.eol("\r\n")
+		p.indent("  ")
+		p.escaper(codelite.esc)
+
+		p.generate(sln, ".workspace", codelite.solution.generate)
+	end
+
+	function codelite.generateProject(prj)
+		p.eol("\r\n")
+		p.indent("  ")
+		p.escaper(codelite.esc)
+
+		if project.iscpp(prj) then
+			p.generate(prj, ".project", codelite.project.generate)
+		end
+	end
+
+	function codelite.cleanSolution(sln)
+		p.clean.file(sln, sln.name .. ".workspace")
+		p.clean.file(sln, sln.name .. "_wsp.mk")
+		p.clean.file(sln, sln.name .. ".tags")
+		p.clean.file(sln, ".clang")
+	end
+
+	function codelite.cleanProject(prj)
+		p.clean.file(prj, prj.name .. ".project")
+		p.clean.file(prj, prj.name .. ".mk")
+		p.clean.file(prj, prj.name .. ".list")
+		p.clean.file(prj, prj.name .. ".out")
+	end
+
+	function codelite.cleanTarget(prj)
+		-- TODO..
+	end
+
+
+	include("_preload.lua")
+	include("codelite_workspace.lua")
+	include("codelite_project.lua")
+
+	return codelite
