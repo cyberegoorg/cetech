47700afa20967782e43f8c5c4e98072b05c82a36
Ondra Voves <o.voves@gmail.com>
2015-08-13 22:07:38 +0200

    rapid json

--- engine/src/runtime/main.cc
+++ engine/src/runtime/main.cc
@@ -7,10 +7,33 @@
 
 #include <iostream>
 
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
 using namespace cetech1;
+using namespace rapidjson;
+
+
 
 void init() {
     memory_globals::init();
+    
+    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    Document d;
+    d.Parse(json);
+
+    // 2. Modify it by DOM.
+    Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    // 3. Stringify the DOM
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << buffer.GetString() << std::endl;
 }
 
 void run() {
