6a11e93d1252222a992d75ff5636d484c9e33d22
Ondra Voves <o.voves@gmail.com>
2015-08-15 20:29:21 +0200

    vector4

---
+++ engine/src/common/math/vector4.h
@@ -0,0 +1,313 @@
+#pragma once
+
+#include "math_types.h"
+#include "math.h"
+
+namespace cetech1 {
+    namespace vector4 {
+        /*! Zero vector [0.0f, 0.0f, 0.0f]
+         */
+        static const Vector4 ZERO = {0.0f, 0.0f, 0.0f, 0.f};
+
+        /*! Unit vector [1.0f, 1.0f, 1.0f]
+         */
+        static const Vector4 UNIT = {1.0f, 1.0f, 1.0f, 1.f};
+
+
+        /*! Up vector [0.0f, 1.0f, 0.0f]
+         */
+        static const Vector4 UP = {0.0f, 1.0f, 0.0f, 0.f};
+
+        /*! Down vector [0.0f, -1.0f, 0.0f]
+         */
+        static const Vector4 DOWN = {0.0f, -1.0f, 0.0f, 0.f};
+
+        /*! Forward vector [0.0f, 0.0f, 1.0f]
+         */
+        static const Vector4 FORWARDS = {0.0f, 0.0f, 1.0f, 0.f};
+
+        /*! Backward vector [0.0f, 0.0f, -1.0f]
+         */
+        static const Vector4 BACKWARDS = {0.0f, 0.0f, -1.0f, 0.f};
+
+        /*! Right vector [1.0f, 0.0f, 0.0f]
+         */
+        static const Vector4 RIGHT = {1.0f, 0.0f, 0.0f, 0.f};
+
+        /*! Left vector [-1.0f, 0.0f, 0.0f]
+         */
+        static const Vector4 LEFT = {-1.0f, 0.0f, 0.0f, 0.f};
+    }
+
+    /*! Vector4 functions.
+     */
+    namespace vector4 {
+        /*! Make Vector4
+         * \param x X
+         * \param y Y
+         * \param z Z
+	 * \param w W
+         * \return New vector
+         */
+        FORCE_INLINE Vector4 make_vector4(float x, float y, float z, float w);
+
+
+        /*! Vector len
+         * \param v1 Vector.
+         * \return Vector len.
+         */
+        FORCE_INLINE float len(const Vector4& v1);
+
+        /*! Vector squared len.
+         * \param v1 Vector.
+         * \return Vector squared len.
+         */
+        FORCE_INLINE float len_sq(const Vector4& v1);
+
+        /*! Vector 1/len
+         * \param v1 Vector.
+         * \return Vector 1/len.
+         */
+        FORCE_INLINE float len_inv(const Vector4& v1);
+
+
+        /*! Return normalized vector.
+         * \param v1 Vector.
+         * \return Normalized vector.
+         */
+        FORCE_INLINE Vector4 normalized(const Vector4& v1);
+
+
+        /*! Dot product.
+         * \param v1 Vector 1.
+         * \param v2 Vector 2.
+         * \return Dot product.
+         */
+        FORCE_INLINE float dot(const Vector4& v1, const Vector4& v2);
+
+        /*! Squared distance between two point.
+         * \param v1 Point 1.
+         * \param v2 Point 2.
+         * \return Squared distace.
+         */
+        FORCE_INLINE float distance_sq(const Vector4& v1, const Vector4& v2);
+
+        /*! Distance between two point.
+         * \param v1 Point 1.
+         * \param v2 Point 2.
+         * \return Distace.
+         */
+        FORCE_INLINE float distance(const Vector4& v1, const Vector4& v2);
+
+
+        /*! Get minimal value from vector element.
+         * \param v1 Vector.
+         * \return Minimal value from vector element.
+         */
+        FORCE_INLINE float min(const Vector4& v1);
+
+        /*! Get maximum value from vector element.
+         * \param v1 Vector.
+         * \return Maximum value from vector element.
+         */
+        FORCE_INLINE float max(const Vector4& v1);
+
+        /*! Get maximum absolute value from vector element.
+         * \param v1 Vector.
+         * \return Absolute maximum value from vector element.
+         */
+        FORCE_INLINE float abs_max(const Vector4& v1);
+
+        /*! Get sign vector.
+         * if element is positive return 1.0f else -1.0f.
+         * \param v1 Vector.
+         * \return Sign vector.
+         */
+        FORCE_INLINE Vector4 sign_vector(const Vector4& v1);
+    }
+
+    /*! Return negativ vector.
+     * \param v1 Vector.
+     * \return -v1
+     */
+    FORCE_INLINE Vector4 operator - (const Vector4 &v1);
+
+
+    /*! V1 + V2
+     * \param v1 Vector1.
+     * \param v2 Vector4.
+     * \return V1 + V2.
+     */
+    FORCE_INLINE Vector4 operator + (const Vector4 &v1, const Vector4 &v2);
+
+    /*! V1 - V2
+     * \param v1 Vector1.
+     * \param v2 Vector4.
+     * \return V1 - V2.
+     */
+    FORCE_INLINE Vector4 operator - (const Vector4 &v1, const Vector4 &v2);
+
+    /*! V1 * V2 [x1 * x2, y1 * y2, z1 * z2]
+     * \param v1 Vector1.
+     * \param v2 Vector4.
+     * \return [x1*x2, y1 * y2, z1 * z2]
+     */
+    FORCE_INLINE Vector4 operator* (const Vector4 &v1, const Vector4 &v2);
+
+    /*! V1 / V2 [x1 / x2, y1 / y2, z1 / z2]
+     * \param v1 Vector1.
+     * \param v2 Vector4.
+     * \return [x1 / x2, y1 / y2, z1 / z2]
+     */
+    FORCE_INLINE Vector4 operator / (const Vector4 &v1, const Vector4 &v2);
+
+
+    /*! V1 * scalar
+     * \param v1 Vector1.
+     * \param s ScalarVector4.
+     * \return V1 * scalar
+     */
+    FORCE_INLINE Vector4 operator* (const Vector4 &v1, const float s);
+
+    /*! V1 / scalar
+     * \param v1 Vector1.
+     * \param s ScalarVector4.
+     * \return V1 / scalar
+     */
+    FORCE_INLINE Vector4 operator / (const Vector4 &v1, const float s);
+
+
+
+    namespace vector4 {
+        FORCE_INLINE Vector4 make_vector4(float x, float y, float z, float w) {
+            return {
+                       x, y, z, w
+            };
+        }
+
+        FORCE_INLINE float len(const Vector4& v1) {
+            return math::fast_sqrt(len_sq(v1));
+        }
+
+        FORCE_INLINE float len_sq(const Vector4& v1) {
+            return dot(v1, v1);
+        }
+
+        FORCE_INLINE float len_inv(const Vector4& v1) {
+            return math::inv_sqrt(len_sq(v1));
+        }
+
+        FORCE_INLINE Vector4 normalized(const Vector4& v1) {
+            return v1 * len_inv(v1);
+        }
+
+        FORCE_INLINE float dot(const Vector4& v1, const Vector4& v2) {
+            return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z)+ (v1.w * v2.w);
+        }
+
+//         FORCE_INLINE Vector4 cross(const Vector4& v1, const Vector4& v2) {
+// 	  (void)v1;
+// 	  (void)v2;
+// 	  // TODO: !!!
+// 	  //             return make_vector4(
+// //                 (v1.y * v2.z) - (v1.z * v2.y),
+// //                 (v1.z * v2.x) - (v1.x * v2.z),
+// //                 (v1.x * v2.y) - (v1.y * v2.x),
+// //                 );
+//         }
+
+        FORCE_INLINE float distance_sq(const Vector4& v1, const Vector4& v2) {
+            return math::square(v2.x - v1.x) + math::square(v2.y - v1.y) + math::square(v2.z - v1.z) + math::square(v2.w - v1.w);
+        }
+
+        FORCE_INLINE float distance(const Vector4& v1, const Vector4& v2) {
+            return math::fast_sqrt(distance_sq(v1, v2));
+        }
+
+        FORCE_INLINE float min(const Vector4& v1) {
+            return math::min(math::min(math::min(v1.x, v1.y), v1.z), v1.w);
+        }
+
+        FORCE_INLINE float max(const Vector4& v1) {
+            return math::max(math::max(math::max(v1.x, v1.y), v1.z), v1.w);
+        }
+
+        FORCE_INLINE float abs_max(const Vector4& v1) {
+            return math::max(math::max(math::max(math::abs(v1.x), math::abs(v1.y)), math::abs(v1.z)), math::abs(v1.w));
+        }
+
+        FORCE_INLINE Vector4 sign_vector(const Vector4& v1) {
+            return make_vector4(
+                math::float_select(v1.x, 1.0f, -1.0f),
+                math::float_select(v1.y, 1.0f, -1.0f),
+                math::float_select(v1.z, 1.0f, -1.0f),
+		math::float_select(v1.w, 1.0f, -1.0f)
+                );
+        }
+    }
+
+    FORCE_INLINE Vector4 operator - (const Vector4 &v1) {
+        return vector4::make_vector4(
+            -v1.x,
+            -v1.y,
+            -v1.z,
+	    -v1.w
+            );
+    }
+
+    FORCE_INLINE Vector4 operator + (const Vector4 &v1, const Vector4 &v2) {
+        return vector4::make_vector4(
+            v1.x + v2.x,
+            v1.y + v2.y,
+            v1.z + v2.z,
+	    v1.w + v2.w
+            );
+    }
+
+    FORCE_INLINE Vector4 operator - (const Vector4 &v1, const Vector4 &v2) {
+        return vector4::make_vector4(
+            v1.x - v2.x,
+            v1.y - v2.y,
+            v1.z - v2.z,
+	    v1.w - v2.w
+            );
+    }
+
+    FORCE_INLINE Vector4 operator* (const Vector4 &v1, const Vector4 &v2) {
+        return vector4::make_vector4(
+            v1.x * v2.x,
+            v1.y * v2.y,
+            v1.z * v2.z,
+	    v1.w * v2.w
+            );
+    }
+
+    FORCE_INLINE Vector4 operator / (const Vector4 &v1, const Vector4 &v2) {
+        return vector4::make_vector4(
+            v1.x / v2.x,
+            v1.y / v2.y,
+            v1.z / v2.z,
+	    v1.w / v2.w
+            );
+    }
+
+    FORCE_INLINE Vector4 operator* (const Vector4 &v1, const float s) {
+        return vector4::make_vector4(
+            v1.x * s,
+            v1.y * s,
+            v1.z * s,
+            v1.w * s
+            );
+    }
+
+    FORCE_INLINE Vector4 operator / (const Vector4 &v1, const float s) {
+        const float inv_s = 1 / s;
+
+        return vector4::make_vector4(
+            v1.x * inv_s,
+            v1.y * inv_s,
+            v1.z * inv_s,
+	    v1.w * inv_s
+            );
+    }
+}
---
+++ engine/tests/common/math/test_vector4.cc
@@ -0,0 +1,131 @@
+#include "catch.hpp"
+
+#include "common/memory/memory.h"
+#include "common/math/vector4.h"
+
+#include "utils.h"
+
+TEST_CASE( "Vector4 operation", "[vector4]" ) {
+    cetech1::Vector4 v1 = {1.0f, 2.0f, 3.0f, 4.0f};
+    cetech1::Vector4 v2 = {5.0f, 6.0f, 7.0f, 8.0f};
+    float scalar = 2.0f;
+
+    SECTION( "v1 + v2" ) {
+        cetech1::Vector4 v3 = v1 + v2;
+
+        REQUIRE( v3.x == 6.0f );
+        REQUIRE( v3.y == 8.0f );
+        REQUIRE( v3.z == 10.0f );
+	REQUIRE( v3.w == 12.0f );
+    }
+
+    SECTION( "v1 - v2" ) {
+        cetech1::Vector4 v3 = v1 - v2;
+
+        REQUIRE( v3.x == -4.0f );
+        REQUIRE( v3.y == -4.0f );
+        REQUIRE( v3.z == -4.0f );
+	REQUIRE( v3.w == -4.0f );
+    }
+
+    SECTION( "v1 * v2" ) {
+        cetech1::Vector4 v3 = v1 * v2;
+
+        REQUIRE( v3.x == 5.0f );
+        REQUIRE( v3.y == 12.0f );
+        REQUIRE( v3.z == 21.0f );
+	REQUIRE( v3.w == 32.0f );
+    }
+
+    SECTION( "v1 / v2" ) {
+        cetech1::Vector4 v3 = v1 / v2;
+
+        REQUIRE_ALMOST( v3.x, 0.2f);
+        REQUIRE_ALMOST( v3.y, 0.33334f);
+        REQUIRE_ALMOST( v3.z, 0.42857f);
+        REQUIRE_ALMOST( v3.w, 0.5f);
+    }
+
+    SECTION( "v1 * scalar" ) {
+        cetech1::Vector4 v2 = v1 * scalar;
+
+        REQUIRE( v2.x == 2.0f );
+        REQUIRE( v2.y == 4.0f );
+    }
+
+    SECTION( "v1 / scalar" ) {
+        cetech1::Vector4 v2 = v1 / scalar;
+
+        REQUIRE( v2.x == 0.5f );
+        REQUIRE( v2.y == 1.0f );
+    }
+
+    SECTION( "len(v1)" ) {
+        float len = cetech1::vector4::len(v1);
+
+        REQUIRE_ALMOST( len, 5.47265f);
+    }
+
+    SECTION( "len_sq(v1)" ) {
+        float len = cetech1::vector4::len_sq(v1);
+
+        REQUIRE( len == 30.0f );
+    }
+
+    SECTION( "len_inv(v1)" ) {
+        float len = cetech1::vector4::len_inv(v1);
+
+        REQUIRE_ALMOST( len, 0.18242f);
+    }
+
+    SECTION( "normalized(v1)" ) {
+        cetech1::Vector4 v_norm = cetech1::vector4::normalized(v1);
+        const float len = cetech1::vector4::len(v_norm);
+
+        REQUIRE_ALMOST3( len, 1.0f, 0.01f); // TODO: check compare
+    }
+
+    SECTION( "dot(v1, v2)" ) {
+        float dot = cetech1::vector4::dot(v1, v2);
+
+        REQUIRE( dot == 70.0f );
+    }
+
+    SECTION( "distance(v1, v2)" ) {
+        float distance = cetech1::vector4::distance(v1, v2);
+
+        REQUIRE_ALMOST( distance, 7.98646f);
+    }
+
+    SECTION( "distance_sq(v1, v2)" ) {
+        float distance_sq = cetech1::vector4::distance_sq(v1, v2);
+
+        REQUIRE( distance_sq == 64.0f );
+    }
+
+    SECTION( "min(v1)" ) {
+        float min = cetech1::vector4::min(v1);
+
+        REQUIRE_ALMOST(min, 1.0f);
+    }
+
+
+    SECTION( "max(v1)" ) {
+        float max = cetech1::vector4::max(v1);
+
+        REQUIRE_ALMOST( max, 4.0f );
+    }
+
+    SECTION( "abs_max(DOWN)" ) {
+        float abs_max = cetech1::vector4::abs_max(cetech1::vector4::DOWN);
+
+        REQUIRE_ALMOST( abs_max, 1.0f);
+    }
+
+    SECTION( "sign_vector(DOWN)" ) {
+        cetech1::Vector4 sign = cetech1::vector4::sign_vector(cetech1::vector4::DOWN);
+
+        REQUIRE_ALMOST( sign.x, 1.0f);
+        REQUIRE_ALMOST( sign.y, -1.0f);
+    }
+}
