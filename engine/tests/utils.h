6e5f79089ac3752286f3c7a7ce53f0e5e982e209
Ondra Voves <o.voves@gmail.com>
2015-08-15 20:15:59 +0200

    vector3

--- engine/tests/utils.h
+++ engine/tests/utils.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define REQUIRE_ALMOST(a, b) REQUIRE(cetech1::math::almost_equal((a), (b)))
-#define REQUIRE_ALMOST3(a, b, e) REQUIRE(cetech1::math::almost_equal((a), (b), (e)))
+#define REQUIRE_ALMOST(a, b) REQUIRE( a == Approx(b))
+#define REQUIRE_ALMOST3(a, b, e) REQUIRE( a == Approx(b).epsilon(e))
