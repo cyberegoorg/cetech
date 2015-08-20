867d8fb18b4f9802bb63a13b1e47ef491db4def6
Ondra Voves <o.voves@gmail.com>
2015-08-20 13:11:23 +0200

    clean main

--- engine/src/runtime/main.cc
+++ engine/src/runtime/main.cc
@@ -20,22 +20,6 @@
 using namespace cetech1;
 using namespace rapidjson;
 
-
-
-void init() {
-    memory_globals::init();
-    runtime::init();
-
-    Window w = runtime::window::make_window(
-        "aaa",
-        runtime::window::WINDOWPOS_CENTERED, runtime::window::WINDOWPOS_CENTERED,
-        800, 600,
-        runtime::window::WINDOW_NOFLAG
-        );
-
-    //runtime::window::destroy_window(w);
-}
-
 void frame_start() {
     runtime::frame_start();
 }
@@ -49,33 +33,18 @@ void run() {
         frame_start();
         frame_end();
     }
+}
 
-    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
-    Document d;
-    d.Parse(json);
+void init() {
+    memory_globals::init();
+    runtime::init();
 
-    // 2. Modify it by DOM.
-    Value& s = d["stars"];
-    s.SetInt(s.GetInt() + 1);
-
-    // 3. Stringify the DOM
-    StringBuffer buffer;
-    Writer < StringBuffer > writer(buffer);
-    d.Accept(writer);
-
-    // Output {"project":"rapidjson","stars":11}
-    std::cout << buffer.GetString() << std::endl;
-
-    const cetech1::Vector2 v = cetech1::vector2::normalized({2.0f, 0.0f});
-
-    std::cout << "len: " << cetech1::vector2::len(v) << ", " << cetech1::vector2::is_normalized(v) << std::endl;
-
-    cetech1::Matrix33 m1 = cetech1::matrix33::IDENTITY;
-    cetech1::Matrix33 m2 = cetech1::matrix33::IDENTITY;
-
-    cetech1::Matrix33 m3 = m1 * m2;
-
-    printf("sqrt: %f, ivn_sqrt: %f\n", cetech1::math::fast_sqrt(4), cetech1::math::fast_inv_sqrt(4));
+    Window w = runtime::window::make_window(
+        "aaa",
+        runtime::window::WINDOWPOS_CENTERED, runtime::window::WINDOWPOS_CENTERED,
+        800, 600,
+        runtime::window::WINDOW_NOFLAG
+        );
 }
 
 void shutdown() {
