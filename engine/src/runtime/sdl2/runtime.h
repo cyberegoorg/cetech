843787ead1eff4606c8f4d2c64de5973a9d0feb0
Ondra Voves <o.voves@gmail.com>
2015-08-21 08:54:28 +0200

    ...

--- engine/src/runtime/sdl2/runtime.h
+++ engine/src/runtime/sdl2/runtime.h
@@ -9,24 +9,27 @@
 
 namespace cetech1 {
     namespace runtime {
-        static uint8_t KeyboardStates[512] = { 0 };
-        static uint8_t KeyboardStatesLast[512] = { 0 };
+        namespace keyboard_internal {
+	    static uint8_t KeyboardStates[512] = { 0 };
+	    static uint8_t KeyboardStatesLast[512] = { 0 };
+	}
 
-
-	static Vector2 MouseAxis = vector2::ZERO;
-	static uint32_t MouseButtonState = 0;
-	static uint32_t MouseButtonStateLast = 0;
-	
-	static uint64_t left_btn_hash = 0;
-	static uint64_t middle_btn_hash = 0;
-	static uint64_t right_btn_hash = 0;
+	namespace mouse_internal {
+	    static Vector2 MouseAxis = vector2::ZERO;
+	    static uint32_t MouseButtonState = 0;
+	    static uint32_t MouseButtonStateLast = 0;
+	    
+	    static uint64_t left_btn_hash = 0;
+	    static uint64_t middle_btn_hash = 0;
+	    static uint64_t right_btn_hash = 0;
+	}
 	
         void init() {
             CE_ASSERT(SDL_Init(SDL_INIT_VIDEO) == 0);
 	    
-	    left_btn_hash = murmur_hash_64("left", strlen("left"), 22);
-	    middle_btn_hash = murmur_hash_64("middle", strlen("middle"), 22);
-	    right_btn_hash = murmur_hash_64("right", strlen("right"), 22);
+	    mouse_internal::left_btn_hash = murmur_hash_64("left", strlen("left"), 22);
+	    mouse_internal::middle_btn_hash = murmur_hash_64("middle", strlen("middle"), 22);
+	    mouse_internal::right_btn_hash = murmur_hash_64("right", strlen("right"), 22);
         }
 
         void shutdown() {
@@ -45,24 +48,24 @@ namespace cetech1 {
             }
 	    
 	    /*Keyboard*/
-            memcpy(KeyboardStates, SDL_GetKeyboardState(NULL), 512);
+            memcpy(keyboard_internal::KeyboardStates, SDL_GetKeyboardState(NULL), 512);
 
 	    /*Mouse*/
 	    int32_t x, y;
-	    MouseButtonState = SDL_GetMouseState(&x, &y);
-	    MouseAxis.x = x;
-	    MouseAxis.y = y;
+	    mouse_internal::MouseButtonState = SDL_GetMouseState(&x, &y);
+	    mouse_internal::MouseAxis.x = x;
+	    mouse_internal::MouseAxis.y = y;
         }
 
         void frame_end() {
-            memcpy(KeyboardStatesLast, KeyboardStates, 512);
-	    MouseButtonStateLast = MouseButtonState;
+            memcpy(keyboard_internal::KeyboardStatesLast, keyboard_internal::KeyboardStates, 512);
+	    mouse_internal::MouseButtonStateLast = mouse_internal::MouseButtonState;
         }
     }
 
     namespace runtime {
         namespace window_internal {
-            CE_INLINE uint32_t pos2sdl_pos(const uint32_t pos) {
+            CE_INLINE uint32_t sdl_pos(const uint32_t pos) {
                 switch (pos) {
                 case window::WINDOWPOS_CENTERED:
                     return SDL_WINDOWPOS_CENTERED;
@@ -75,7 +78,7 @@ namespace cetech1 {
                 }
             }
 
-            CE_INLINE uint32_t flags2sdl_flags(const window::WindowFlags flags) {
+            CE_INLINE uint32_t sdl_flags(const window::WindowFlags flags) {
                 uint32_t sdl_flags = 0;
 
                 if (flags & window::WINDOW_FULLSCREEN) {
@@ -97,9 +100,9 @@ namespace cetech1 {
 
                 res.wnd = SDL_CreateWindow(
                     title,
-                    window_internal::pos2sdl_pos(x), window_internal::pos2sdl_pos(y),
+                    window_internal::sdl_pos(x), window_internal::sdl_pos(y),
                     width, height,
-                    window_internal::flags2sdl_flags(flags)
+                    window_internal::sdl_flags(flags)
                     );
 
                 return res;
@@ -128,58 +131,62 @@ namespace cetech1 {
             }
 
             bool button_state(const uint32_t button_index) {
-                return KeyboardStates[button_index];
+                return keyboard_internal::KeyboardStates[button_index];
             }
 
             bool button_pressed(const uint32_t button_index) {
-                return !KeyboardStatesLast[button_index] && KeyboardStates[button_index];
+                return !keyboard_internal::KeyboardStatesLast[button_index] && keyboard_internal::KeyboardStates[button_index];
             }
 
             bool button_released(const uint32_t button_index) {
-                return KeyboardStatesLast[button_index] && !KeyboardStates[button_index];
+                return keyboard_internal::KeyboardStatesLast[button_index] && !keyboard_internal::KeyboardStates[button_index];
             }
         };
 	
 
         namespace mouse {
             uint32_t button_index(const char* scancode) {
-		uint64_t h = murmur_hash_64(scancode, strlen(scancode), 22);
-		
-		if (h == left_btn_hash)
+		const uint64_t h = murmur_hash_64(scancode, strlen(scancode), 22);
+		if (h == mouse_internal::left_btn_hash)
 		    return SDL_BUTTON_LMASK;
-		else if (h == middle_btn_hash)
+		else if (h == mouse_internal::middle_btn_hash)
 		    return SDL_BUTTON_MMASK;
-		else if (h == right_btn_hash)
+		else if (h == mouse_internal::right_btn_hash)
 		    return SDL_BUTTON_RMASK;
 		
 		return 0;
 	    }
 
             const char* button_name(const uint32_t button_index) {
-		if (button_index == SDL_BUTTON_LMASK)
+		switch(button_index) {
+		    case SDL_BUTTON_LMASK:
 			return "left";
-		else if (button_index == SDL_BUTTON_MMASK)
+			
+		    case SDL_BUTTON_MMASK:
 			return "middle";
-		else if (button_index == SDL_BUTTON_RMASK)
+			
+		    case SDL_BUTTON_RMASK:
 			return "right";
-
-		return "";
+		    
+		    default:
+			return "";
+		}
 	    }
 
             bool button_state(const uint32_t button_index) {
-		return MouseButtonState & button_index;
+		return mouse_internal::MouseButtonState & button_index;
 	    }
 	    
             bool button_pressed(const uint32_t button_index) {
-		return !(MouseButtonStateLast & button_index) && (MouseButtonState & button_index);
+		return !(mouse_internal::MouseButtonStateLast & button_index) && (mouse_internal::MouseButtonState & button_index);
 	    }
 	    
             bool button_released(const uint32_t button_index){
-		return (MouseButtonStateLast & button_index) && !(MouseButtonState & button_index);
+		return (mouse_internal::MouseButtonStateLast & button_index) && !(mouse_internal::MouseButtonState & button_index);
 	    }
 
 	    Vector2 axis() {
-		return MouseAxis;
+		return mouse_internal::MouseAxis;
 	    }
         };
 	
