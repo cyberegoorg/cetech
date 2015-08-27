f8dbad4a61016581076cab9f37463ed6f5f72544
Ondra Voves <o.voves@gmail.com>
2015-08-27 21:18:18 +0200

    handlers test

--- engine/src/common/ecs/entitymanager.h
+++ engine/src/common/ecs/entitymanager.h
@@ -4,27 +4,27 @@
 #include "common/memory/memory_types.h"
 
 #include "common/types.h"
-#include "common/handler.h"
+#include "common/handlermanager.h"
 
 #include "common/memory/memory.h"
 #include "common/container/queue.h"
 
-namespace cetech {   
+namespace cetech {
     namespace entity_manager_internal {
-        typename handler::Handler<uint32_t,uint32_t, unsigned char,22,8,1024> entity_handler;
+        typename handler::HandlerManager<uint32_t,uint32_t, unsigned char,22,8,1024> entity_handler_manager;
     }
 
     namespace entity_manager {
         Entity create(EntityManager& em) {
-            return {entity_manager_internal::entity_handler.create(em._free_idx, em._generation)};
+            return {entity_manager_internal::entity_handler_manager.create(em._free_idx, em._generation)};
         }
 
         void destroy(EntityManager& em, const Entity e) {
-            entity_manager_internal::entity_handler.destroy(e.id, em._free_idx, em._generation);
+            entity_manager_internal::entity_handler_manager.destroy(e.id, em._free_idx, em._generation);
         }
 
         bool alive(const EntityManager& em, const Entity e) {
-            return entity_manager_internal::entity_handler.alive(e.id, em._generation);
+            return entity_manager_internal::entity_handler_manager.alive(e.id, em._generation);
         }
     }
 }
