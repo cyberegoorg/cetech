b47acba9d47309519daafdaadd1199e269ed3d4c
Ondra Voves <o.voves@gmail.com>
2015-09-01 09:24:34 +0200

    some cvar refactor

--- engine/src/common/cvars.h
+++ engine/src/common/cvars.h
@@ -4,7 +4,7 @@
 
 namespace cetech {
     namespace cvars {
-        static CVar rm_build_dir("rm.build_dir", "Path to build data dir.", "./data/build/", CVar::FLAG_CONST);
-        static CVar rm_source_dir("rm.source_dir", "Path to source data dir.", "./data/src/", CVar::FLAG_CONST);
+        extern CVar rm_build_dir;
+        extern CVar rm_source_dir;
     }
 }
