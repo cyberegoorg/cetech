c4531774a1b65dd19562b160022382436ffe4833
Ondra Voves <o.voves@gmail.com>
2015-09-09 22:59:00 +0200

    console widget

---
+++ tools/src/console/consolewidget.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QFrame
+
+from console.ui.consolewidget import Ui_ConsoleWidget
+
+
+class ConsoleWidget(QFrame, Ui_ConsoleWidget):
+    LOG_COLOR_TAG = {
+        'I': "%s",
+        'W': "<span style='color: yellow;'> %s</span>",
+        'D': "%s",
+        'E': "<span style='color: red;'> %s</span>",
+    }
+
+    def __init__(self, api):
+        super(ConsoleWidget, self).__init__()
+        self.setupUi(self)
+
+        self.api = api
+        self.api.register_on_log_handler(self.add_log)
+
+    def add_log(self, level, where, message):
+        if 'lua' in where:
+            self.command_log_textedit.append(self.LOG_COLOR_TAG[level] % message)
+
+    def send_command(self):
+        text = self.command_lineedit.text()
+
+        if len(text) > 0:
+            self.api.lua_execute(text)
+            self.command_log_textedit.append("> %s" % text)
+
+        self.command_lineedit.setText("")
--- tools/src/console/mainwindow.py
+++ tools/src/console/mainwindow.py
@@ -1,20 +1,56 @@
+import argparse
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import QMainWindow, QTreeWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QTreeWidgetItem, QStyle
 
 from cetech.api import ConsoleAPI
+from console.consolewidget import ConsoleWidget
 from console.ui.mainwindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+    LOG_COLOR = {
+        'I': QColor("blue"),
+        'W': QColor("yellow"),
+        'D': QColor("green"),
+        'E': QColor("red"),
+    }
+
+    LOG_ICON = {
+        'I': QStyle.SP_MessageBoxInformation,
+        'W': QStyle.SP_MessageBoxWarning,
+        'D': QStyle.SP_MessageBoxQuestion,
+        'E': QStyle.SP_MessageBoxCritical,
+    }
+
+    LOG_COLOR_TAG = {
+        'I': "%s",
+        'W': "<span style='color: yellow;'> %s</span>",
+        'D': "%s",
+        'E': "<span style='color: red;'> %s</span>",
+    }
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
         self.centralwidget.hide()
 
-        self.api = ConsoleAPI('localhost', 2222)
+        self.parser = argparse.ArgumentParser("cetech_console")
+        self.parser.add_argument("-d", "--data-dir", type=str, help="data dir")
+        self.parser.add_argument("-a", "--console-address", type=str, help="console address", default='localhost')
+        self.parser.add_argument("-p", "--console-port", type=int, help="console port", default=2222)
+        self.args = self.parser.parse_args()
+
+        self.data_dir = self.args.data_dir
+        self.console_port = self.args.console_port
+        self.console_address = self.args.console_address
+
+        self.api = ConsoleAPI(self.console_address, self.console_port)
         self.api.register_on_log_handler(self.add_log)
+
+        self.console_widget = ConsoleWidget(self.api)
+        self.console_dock_widget.setWidget(self.console_widget)
 
         self.console_api_timer = QTimer()
         self.console_api_timer.timeout.connect(self.api.tick)
@@ -25,40 +61,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.statusbar.showMessage("Disconnecting ...")
 
-        while(self.api.connect):
+        while (self.api.connect):
             self.api.tick()
 
         self.statusbar.showMessage("Disconnected")
 
         super(MainWindow, self).closeEvent(evnt)
 
-
-    LOG_COLOR = {
-        'I': QColor("blue"),
-        'W': QColor("yellow"),
-        'D': QColor("green"),
-        'E': QColor("red"),
-    }
-
-    LOG_COLOR_TAG = {
-        'I': "%s",
-        'W': "<span style='color: yellow;'> %s</span>",
-        'D': "%s",
-        'E': "<span style='color: red;'> %s</span>",
-    }
-
     def add_log(self, level, where, message):
         item = QTreeWidgetItem([level, where, message])
+
+        item.setIcon(0, self.style().standardIcon(self.LOG_ICON[level]))
+
         self.LogWidget.addTopLevelItem(item)
-
-        if where == 'lua':
-            self.command_log_textedit.append(self.LOG_COLOR_TAG[level] % message)
-
-    def send_command(self):
-        text = self.command_lineedit.text()
-
-        if len(text) > 0:
-            self.api.lua_execute(text)
-            self.command_log_textedit.append("> %s" % text)
-
-        self.command_lineedit.setText("")
