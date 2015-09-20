99a1e45e0553d03b7c9ef6ec12a6e4b08687bf2e
Ondra Voves <o.voves@gmail.com>
2015-09-20 21:00:59 +0200

    ..wip

--- engine/src/common/eventstream/eventstream.h
+++ engine/src/common/eventstream/eventstream.h
@@ -4,6 +4,7 @@
 #include "common/container/hash.h"
 #include "common/console_server/console_server.h"
 #include "common/eventstream/eventstream_types.h"
+#include <runtime/runtime.h>
 
 #include "rapidjson/document.h"
 
@@ -13,14 +14,15 @@ namespace cetech {
         CE_INLINE void clear(EventStream& stream);
 
         CE_INLINE EventStreamHeader* header(EventStream& stream, event_it event);
-        template < typename T > T* data(EventStream& stream, event_it event);
+        template < typename T > T* data_ptr(EventStream& stream, event_it event);
 
         CE_INLINE bool empty(EventStream& stream);
 
         CE_INLINE event_it first(EventStream& stream);
         CE_INLINE event_it next(EventStream& stream, event_it event);
+        CE_INLINE bool is_valid(EventStream& stream, event_it event);
 
-        template < typename T > T* prepare_new(EventStream& stream, uint32_t type);
+        template < typename T > T* create(EventStream& stream, uint32_t type);
 
         CE_INLINE void add_events(EventStream& stream, event_t* events, uint32_t size);
     }
@@ -36,7 +38,7 @@ namespace cetech {
         }
 
         template < typename T >
-        T* data(EventStream& stream, event_it event) {
+        T* data_ptr(EventStream& stream, event_it event) {
             return (T*)(&(header(stream, event)->data));
         }
 
@@ -59,6 +61,10 @@ namespace cetech {
             return n;
         }
 
+        bool is_valid(EventStream& stream, event_it event) {
+            return event != end(stream);
+        }
+
         template < typename T >
         T* prepare_new(EventStream& stream, uint32_t type) {
             event_it it = end(stream);
@@ -75,12 +81,18 @@ namespace cetech {
 
 
         void add_events(EventStream& stream, event_t* events, uint32_t size) {
+            runtime::thread::spin_lock(&stream.lock);
+
             event_t* first = array::end(stream.stream);
             array::resize(stream.stream, array::size(stream.stream) + size);
 
             memcpy(first, events, size);
+
+            runtime::thread::spin_unlock(&stream.lock);
         };
     }
 
-    EventStream::EventStream ( Allocator& allocator )  : stream(allocator) {}
+    EventStream::EventStream ( Allocator& allocator, const uint32_t init_size )  : stream(allocator) {
+        array::reserve(stream, init_size);
+    }
 };
