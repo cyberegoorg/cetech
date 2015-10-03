2f01e75c92d3798dfd4e54e626f9a13f6a845c6b
Ondra Voves <o.voves@gmail.com>
2015-10-03 04:59:37 +0200

    from_cstring_len => from_cstringn

--- engine/src/common/string/stringid.inl.h
+++ engine/src/common/string/stringid.inl.h
@@ -6,12 +6,12 @@
 
 namespace cetech {
     namespace stringid64 {
-        CE_INLINE StringId64_t from_cstring_len(const char* str, const uint32_t len, const uint32_t seed = 22);
+        CE_INLINE StringId64_t from_cstringn(const char* str, const uint32_t len, const uint32_t seed = 22);
         CE_INLINE StringId64_t from_cstring(const char* str, const uint32_t seed = 22);
     }
 
     namespace stringid64 {
-        StringId64_t from_cstring_len(const char* str, const uint32_t len, const uint32_t seed) {
+        StringId64_t from_cstringn(const char* str, const uint32_t len, const uint32_t seed) {
             CE_CHECK_PTR(str);
 
             return murmur_hash_64(str, len, seed);
@@ -20,7 +20,7 @@ namespace cetech {
         StringId64_t from_cstring(const char* str, const uint32_t seed) {
             CE_CHECK_PTR(str);
 
-            return from_cstring_len(str, strlen(str), seed);
+            return from_cstringn(str, strlen(str), seed);
         }
     }
 }
