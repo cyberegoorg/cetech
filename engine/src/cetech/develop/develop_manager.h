8565cfbd47836aad3ad72b36a9964b3f8e52afe7
Ondra Voves <o.voves@gmail.com>
2015-10-24 23:38:51 +0200

    develop manager refactor

--- engine/src/cetech/develop/develop_manager.h
+++ engine/src/cetech/develop/develop_manager.h
@@ -5,18 +5,49 @@
 #include "rapidjson/document.h"
 
 namespace cetech {
-    class DevelopManager {
-        public:
-            virtual void clear() = 0;
-            virtual void send_buffer() = 0;
+    namespace develop_manager {
+        enum EventType {
+            EVENT_NONE = 0,
 
-            virtual EventStream& event_stream() = 0;
+            EVENT_BEGIN_FRAME = 1,
+            EVENT_END_FRAME = 2,
 
-            virtual void push_begin_frame() = 0;
-            virtual void push_end_frame() = 0;
-            virtual void push_record_float(const char* name, const float value) = 0;
+            EVENT_RECORD_FLOAT = 3,
 
-            static DevelopManager* make(Allocator& allocator);
-            static void destroy(Allocator& allocator, DevelopManager* dm);
-    };
+            EVENT_MEMORY_ALLOC = 4,
+            EVENT_MEMORY_FREE = 5,
+
+            EVENT_COUNT
+        };
+
+        struct BeginFrameEvent {
+            uint32_t get_frame_id;
+            uint32_t time;
+        };
+
+        struct EndFrameEvent {
+            uint32_t get_frame_id;
+            uint32_t time;
+        };
+
+        struct RecordFloatEvent {
+            const char* name;
+            uint32_t get_frame_id;
+            float value;
+        };
+
+        void clear();
+        void send_buffer();
+
+        EventStream& event_stream();
+
+        void push_begin_frame();
+        void push_end_frame();
+        void push_record_float(const char* name, const float value);
+    }
+
+    namespace develop_manager_globals {
+        void init();
+        void shutdown();
+    }
 }
