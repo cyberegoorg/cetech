656a75e0c96a52bd7e7f238cf1e0645c25946a8e
Ondra Voves <o.voves@gmail.com>
2015-10-27 04:02:18 +0100

    assert print stacktrace

--- engine/src/celib/memory/memory.cc
+++ engine/src/celib/memory/memory.cc
@@ -1,69 +1,10 @@
 // based on bitsquid foundation
-#include "memory.h"
-#include <celib/container/hash.inl.h>
-
+#include "celib/memory/memory.h"
+#include "celib/stacktrace.h"
 #include <stdlib.h>
 #include <assert.h>
 #include <cstdint>
 
-
-#include <execinfo.h>
-#include <cxxabi.h>
-
-static char* c_stacktrace( int skip = 2) {
-    char* return_str = (char*)malloc(4096 * 8);
-    return_str[0] = '\0';
-
-    void* array[50];
-    int size = backtrace(array, 50);
-
-    char** messages = backtrace_symbols(array, size);
-
-    for (int i = skip; i < size && messages != NULL; ++i) {
-        char* mangled_name = 0, * offset_begin = 0, * offset_end = 0;
-
-        for (char* p = messages[i]; *p; ++p) {
-            if (*p == '(') {
-                mangled_name = p;
-            } else if (*p == '+') {
-                offset_begin = p;
-            } else if (*p == ')') {
-                offset_end = p;
-                break;
-            }
-        }
-
-        if (mangled_name && offset_begin && offset_end && mangled_name < offset_begin) {
-            *mangled_name++ = '\0';
-            *offset_begin++ = '\0';
-            *offset_end++ = '\0';
-
-            int status;
-            char* real_name = abi::__cxa_demangle(mangled_name, 0, 0, &status);
-
-            if (status == 0) {
-                char buffer[4096] = {0};
-                sprintf(buffer,
-                        "\t[%d] %s: (%s)+%s %s\n",
-                        i,
-                        messages[i],
-                        (status == 0 ? real_name : mangled_name),
-                        offset_begin,
-                        offset_end);
-                strcat(return_str, buffer);
-                free(real_name);
-            }
-        } else {
-            char buffer[4096] = {0};
-            sprintf(buffer, "\t[%d] %s\n", i, messages[i]);
-            strcat(return_str, buffer);
-        }
-    }
-
-    free(messages);
-
-    return return_str;
-}
 
 namespace {
     using namespace cetech;
@@ -148,7 +89,8 @@ namespace {
                         continue;
                     }
 
-                    fprintf(stderr,"[MEMORY LEAK][%p] allocate traceback:\n%s", mem_trace[i].p, mem_trace[i].traceback);
+                    fprintf(stderr, "[MEMORY LEAK][%p] allocate traceback:\n%s", mem_trace[i].p,
+                            mem_trace[i].traceback);
                     deallocate(mem_trace[i].p);
                 }
 
@@ -164,7 +106,7 @@ namespace {
                 _total_allocated += ts;
 
 
-                const char* trace = c_stacktrace();
+                const char* trace = stacktrace(2);
                 add_entry(p, trace);
                 return p;
             }
