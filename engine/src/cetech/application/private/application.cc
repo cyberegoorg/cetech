7def07142cce929c64851e90f2b92a7763e95f2d
Ondra Voves <o.voves@gmail.com>
2015-11-01 23:28:06 +0100

    ... (#62)

--- engine/src/cetech/application/private/application.cc
+++ engine/src/cetech/application/private/application.cc
@@ -294,10 +294,10 @@ namespace cetech {
 
                 task_manager::add_end(task_end, sizeof(task_end) / sizeof(task_manager::TaskID));
                 task_manager::wait(frame_task);
-                
-                if(task_manager::open_task_count() != 0) {
-                    printf("c: %d \n", task_manager::open_task_count());
-                }
+
+                //                 if(task_manager::open_task_count() != 0) {
+                //                     printf("c: %d \n", task_manager::open_task_count());
+                //                 }
 
                 CE_ASSERT(task_manager::open_task_count() == 0);
                 usleep(16 * 1000);
