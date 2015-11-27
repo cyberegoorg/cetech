f2066250ec35f469e4257b528c9e5c59d8a91d3b
Ondra Voves <o.voves@gmail.com>
2015-11-27 13:05:37 +0100

    darwin64, #112

--- engine/src/celib/time.h
+++ engine/src/celib/time.h
@@ -1,6 +1,19 @@
 #pragma once
 
+#ifdef __MACH__
+#include <sys/time.h>
+//clock_gettime is not implemented on OSX
+int clock_gettime(int /*clk_id*/, struct timespec* t) {
+    struct timeval now;
+    int rv = gettimeofday(&now, NULL);
+    if (rv) return rv;
+    t->tv_sec  = now.tv_sec;
+    t->tv_nsec = now.tv_usec * 1000;
+    return 0;
+}
+#else
 #include <ctime>
+#endif
 
 namespace cetech {
     namespace time {
