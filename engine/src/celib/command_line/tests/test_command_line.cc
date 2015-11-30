73a19af1a0c1491299b61c83e2c92762e24436df
Ondra Voves <o.voves@gmail.com>
2015-11-30 21:31:01 +0100

    command_line test

--- engine/src/celib/command_line/tests/test_command_line.cc
+++ engine/src/celib/command_line/tests/test_command_line.cc
@@ -1,5 +1,9 @@
 #ifdef CETECH_TEST
 
+/*******************************************************************************
+**** Includes
+*******************************************************************************/
+
 #include "catch/catch.hpp"
 
 #include "celib/memory/memory.h"
@@ -7,26 +11,40 @@
 
 using namespace cetech;
 
-TEST_CASE( "Parse command line arguments", "[command_line]" ) {
+/*******************************************************************************
+**** Array can store items
+*******************************************************************************/
+SCENARIO( "comand_line can parse comand line =D", "[command_line]" ) {
     memory_globals::init();
 
-    const char* argv[] = {
-        "foo",
-        "-v",
-        "--path",
-        "foo/bar"
-    };
+    GIVEN( "args: foo -v --path foo/bar" ) {
+        const char* argv[] = {
+            "foo",
+            "-v",
+            "--path",
+            "foo/bar"
+        };
 
-    const int argc = sizeof(argv) / sizeof(char*);
-    command_line::set_args(argc, argv);
+        const int argc = sizeof(argv) / sizeof(char*);
+        command_line::set_args(argc, argv);
+        
+        WHEN( "find args --path -p" ) {
+            THEN( "has argument" ) {
+                REQUIRE( command_line::has_argument("path", 'p'));
+            }
 
-    REQUIRE( command_line::has_argument("path", 'p'));
-    REQUIRE( command_line::has_argument("verbose", 'v'));
+            AND_THEN("index is 2") {
+                REQUIRE( command_line::find_argument("path", 'p') == 2);
+            }
 
-    REQUIRE( command_line::find_argument("path", 'p') == 2);
-    REQUIRE( command_line::find_argument("verbose", 'v') == 1 );
+            AND_THEN("parametr is foo/bar") {
+                REQUIRE( command_line::get_parameter("path", 'p') == "foo/bar" );
+            }
+        }
+    }
 
-    REQUIRE( command_line::get_parameter("path", 'p') == "foo/bar" );
+
+
 
     memory_globals::shutdown();
 }
