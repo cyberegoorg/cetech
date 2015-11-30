55a7fbdfc490da94994ac1e74f3722f97f0f2a53
Ondra Voves <o.voves@gmail.com>
2015-11-30 21:12:09 +0100

    add array iterate test, uncrustify

--- engine/src/celib/command_line/tests/test_command_line.cc
+++ engine/src/celib/command_line/tests/test_command_line.cc
@@ -17,17 +17,17 @@ TEST_CASE( "Parse command line arguments", "[command_line]" ) {
         "foo/bar"
     };
 
-    const int argc = sizeof(argv)/sizeof(char*);
+    const int argc = sizeof(argv) / sizeof(char*);
     command_line::set_args(argc, argv);
 
-    REQUIRE( command_line::has_argument("path", 'p') );
-    REQUIRE( command_line::has_argument("verbose", 'v') );
-    
+    REQUIRE( command_line::has_argument("path", 'p'));
+    REQUIRE( command_line::has_argument("verbose", 'v'));
+
     REQUIRE( command_line::find_argument("path", 'p') == 2);
     REQUIRE( command_line::find_argument("verbose", 'v') == 1 );
-    
+
     REQUIRE( command_line::get_parameter("path", 'p') == "foo/bar" );
-    
+
     memory_globals::shutdown();
 }
 #endif
--- engine/src/celib/container/tests/test_array.cc
+++ engine/src/celib/container/tests/test_array.cc
@@ -1,25 +1,39 @@
 #ifdef CETECH_TEST
 
+/*******************************************************************************
+**** Includes
+*******************************************************************************/
+
 #include "catch/catch.hpp"
 
 #include "celib/memory/memory.h"
 #include "celib/container/array.inl.h"
 
+using namespace cetech;
+
+/*******************************************************************************
+**** Array can store items
+*******************************************************************************/
 SCENARIO( "Array can store items", "[array]" ) {
-    cetech::memory_globals::init();
+    memory_globals::init();
 
     GIVEN( "A empty array" ) {
-        cetech::Array < int > a(cetech::memory_globals::default_allocator());
+        Array < int > a(memory_globals::default_allocator());
 
-        REQUIRE( cetech::array::size(a) == 0 );
+        REQUIRE( array::size(a) == 0 );
+        REQUIRE( array::empty(a));
 
         WHEN( "some items push back" ) {
-            cetech::array::push_back(a, 1);
-            cetech::array::push_back(a, 2);
-            cetech::array::push_back(a, 3);
+            array::push_back(a, 1);
+            array::push_back(a, 2);
+            array::push_back(a, 3);
 
             THEN( "size changed" ) {
-                REQUIRE( cetech::array::size(a) == 3 );
+                REQUIRE( array::size(a) == 3 );
+            }
+
+            AND_THEN("array has any item") {
+                REQUIRE( array::any(a));
             }
 
             AND_THEN("item at index 0 is first pushed") {
@@ -37,60 +51,107 @@ SCENARIO( "Array can store items", "[array]" ) {
 
     }
 
-    cetech::memory_globals::shutdown();
+    memory_globals::shutdown();
 }
 
+/*******************************************************************************
+**** Array can be sized and resized
+*******************************************************************************/
 SCENARIO( "Array can be sized and resized", "[array]" ) {
-    cetech::memory_globals::init();
+    memory_globals::init();
 
     GIVEN( "A array with some items" ) {
-        cetech::Array < int > a(cetech::memory_globals::default_allocator());
+        Array < int > a(memory_globals::default_allocator());
 
-        cetech::array::push_back(a, 0);
-        cetech::array::push_back(a, 1);
-        cetech::array::push_back(a, 2);
+        array::push_back(a, 0);
+        array::push_back(a, 1);
+        array::push_back(a, 2);
 
-        REQUIRE( cetech::array::size(a) == 3 );
+        REQUIRE( array::size(a) == 3 );
         REQUIRE( a._capacity >= 8 );
 
         WHEN( "size is increased" ) {
-            cetech::array::resize(a, 10);
+            array::resize(a, 10);
 
             THEN( "the size and capacity change" ) {
-                REQUIRE( cetech::array::size(a) == 10 );
+                REQUIRE( array::size(a) == 10 );
                 REQUIRE( a._capacity >= 10 );
             }
         }
 
         WHEN( "size is reduced" ) {
-            cetech::array::resize(a, 5);
+            array::resize(a, 5);
 
             THEN( "the size change but not capacity" ) {
-                REQUIRE( cetech::array::size(a) == 5 );
+                REQUIRE( array::size(a) == 5 );
                 REQUIRE( a._capacity >= 5 );
             }
         }
 
         WHEN( "more capacity is reserved" ) {
-            cetech::array::reserve(a, 10);
+            array::reserve(a, 10);
 
             THEN( "the capacity change but not size" ) {
-                REQUIRE( cetech::array::size(a) == 3 );
+                REQUIRE( array::size(a) == 3 );
                 REQUIRE( a._capacity >= 10 );
             }
         }
 
         WHEN( "less capacity is reserved" ) {
-            cetech::array::reserve(a, 0);
+            array::reserve(a, 0);
 
             THEN( "the capacity change but not size" ) {
-                REQUIRE( cetech::array::size(a) == 3 );
+                REQUIRE( array::size(a) == 3 );
                 REQUIRE( a._capacity >= 5 );
             }
         }
     }
 
-    cetech::memory_globals::shutdown();
+    memory_globals::shutdown();
+}
+
+/*******************************************************************************
+**** Can iterate.
+*******************************************************************************/
+SCENARIO( "Can iterate array", "[array]" ) {
+    memory_globals::init();
+
+    GIVEN( "Array with elements [1, 2, 3, 4, 5]" ) {
+        Array < int > a(memory_globals::default_allocator());
+
+        static const int items [] = {1, 2, 3, 4, 5};
+        array::push(a, items, 5);
+
+        REQUIRE( array::size(a) == 5 );
+
+        WHEN( "iterate from begin to end" ) {
+            int* it = array::begin(a);
+
+            THEN( "item is 1, 2, 3, 4, 5, end" ) {
+                REQUIRE( *it == 1 );
+                REQUIRE( *++it == 2 );
+                REQUIRE( *++it == 3 );
+                REQUIRE( *++it == 4 );
+                REQUIRE( *++it == 5 );
+                REQUIRE( ++it == array::end(a));
+            }
+        }
+
+        WHEN( "iterate from end to begin" ) {
+            int* it = array::end(a);
+
+            THEN( "item is 5, 4, 3, 2, begin" ) {
+                REQUIRE( *--it == 5 );
+                REQUIRE( *--it == 4 );
+                REQUIRE( *--it == 3 );
+                REQUIRE( *--it == 2 );
+                REQUIRE( --it == array::begin(a));
+            }
+        }
+
+    }
+
+    memory_globals::shutdown();
 }
 
 
