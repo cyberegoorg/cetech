174f95b182aa81b6356fb8fb1ea563d492fefe13
Ondra Voves <o.voves@gmail.com>
2015-11-30 21:12:09 +0100

    add array iterate test, uncrustify

--- engine/src/celib/command_line/private/command_line.cc
+++ engine/src/celib/command_line/private/command_line.cc
@@ -4,6 +4,22 @@ namespace cetech {
     namespace internal {
         static int _argc;
         static const char** _argv;
+
+        inline bool is_short(const int i,
+                             char shortopt) {
+            return (shortopt != '\0' &&
+                    strlen(internal::_argv[i]) > 1 &&
+                    internal::_argv[i][0] == '-' &&
+                    internal::_argv[i][1] == shortopt);
+        }
+
+        inline bool is_long(const int i,
+                            const char* longopt) {
+            return (strlen(internal::_argv[i]) > 2 &&
+                    internal::_argv[i][0] == '-' &&
+                    internal::_argv[i][1] == '-' &&
+                    strcmp(&internal::_argv[i][2], longopt) == 0);
+        }
     }
 
     namespace command_line {
@@ -16,10 +32,8 @@ namespace cetech {
         int find_argument(const char* longopt,
                           char shortopt) {
             for (int i = 0; i < internal::_argc; i++) {
-                if ((shortopt != '\0' && strlen(internal::_argv[i]) > 1 && internal::_argv[i][0] == '-' &&
-                     internal::_argv[i][1] == shortopt) ||
-                    (strlen(internal::_argv[i]) > 2 && internal::_argv[i][0] == '-' && internal::_argv[i][1] == '-' &&
-                     strcmp(&internal::_argv[i][2], longopt) == 0)) {
+                if (internal::is_short(i, shortopt) ||
+                    internal::is_long(i, longopt)) {
                     return i;
                 }
             }
