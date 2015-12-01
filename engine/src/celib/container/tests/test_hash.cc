7e8726985d5187e895f3ca53dc495787e5c591bf
Ondra Voves <o.voves@gmail.com>
2015-12-01 20:57:28 +0100

    multi_hash tests

--- engine/src/celib/container/tests/test_hash.cc
+++ engine/src/celib/container/tests/test_hash.cc
@@ -12,9 +12,13 @@
 using namespace cetech;
 
 /*******************************************************************************
+**** Hash
+*******************************************************************************/
+
+/*******************************************************************************
 **** Hash can map value to key
 *******************************************************************************/
-SCENARIO( "Hash can map value to key", "[hash_map]" ) {
+SCENARIO( "Hash can map value to key", "[hash]" ) {
     memory_globals::init();
 
     GIVEN( "Empty hash" ) {
@@ -43,7 +47,7 @@ SCENARIO( "Hash can map value to key", "[hash_map]" ) {
 /*******************************************************************************
 **** Hash can remove key
 *******************************************************************************/
-SCENARIO( "Hash can remove key", "[hash_map]" ) {
+SCENARIO( "Hash can remove key", "[hash]" ) {
     memory_globals::init();
 
     GIVEN( "Map 1 -> 10, 2 -> 20, 3 -> 30" ) {
@@ -70,4 +74,80 @@ SCENARIO( "Hash can remove key", "[hash_map]" ) {
 
     memory_globals::shutdown();
 }
+
+/*******************************************************************************
+**** Multi hash
+*******************************************************************************/
+
+/*******************************************************************************
+**** Multi hash can map value to keys
+*******************************************************************************/
+SCENARIO( "Hash can map value to keys", "[multi_hash]" ) {
+    memory_globals::init();
+
+    GIVEN( "Empty hash" ) {
+        Hash < int > hash_map(memory_globals::default_allocator());
+
+        WHEN( "Map 1 -> 10, 1 -> 20, 3 -> 30") {
+            multi_hash::insert(hash_map, 1, 10);
+            multi_hash::insert(hash_map, 1, 20);
+            multi_hash::insert(hash_map, 3, 30);
+
+            const Hash<int>::Entry* entry = multi_hash::find_first(hash_map, 1);
+            THEN( "First item has value 20" ) {
+                REQUIRE(entry->value == 20);
+
+            } AND_THEN( "Second item has value 10" ) {
+                entry = multi_hash::find_next(hash_map, entry);
+                REQUIRE(entry->value == 10);
+
+            }
+        }
+    }
+
+    memory_globals::shutdown();
+}
+
+/*******************************************************************************
+**** Can remove item from multi hash
+*******************************************************************************/
+SCENARIO( "Can remove item from multi hash", "[multi_hash]" ) {
+    memory_globals::init();
+
+    GIVEN( "Map 1 -> 10, 1 -> 20, 3 -> 30" ) {
+        Hash < int > hash_map(memory_globals::default_allocator());
+
+        multi_hash::insert(hash_map, 1, 10);
+        multi_hash::insert(hash_map, 1, 20);
+        multi_hash::insert(hash_map, 3, 30);
+
+        WHEN("Remove key 3") {
+            const Hash<int>::Entry* entry = multi_hash::find_first(hash_map, 3);
+            multi_hash::remove(hash_map, entry);
+
+            THEN("key 3 does not have values" ) {
+                REQUIRE(multi_hash::count(hash_map, 3) == 0);
+
+            }
+
+        } AND_WHEN("Remove key 1") {
+            const Hash<int>::Entry* entry = multi_hash::find_first(hash_map, 1);
+            multi_hash::remove(hash_map, entry);
+
+            THEN( "key 1 has one value" ) {
+                REQUIRE(multi_hash::count(hash_map, 1) == 1);
+            }
+
+        } AND_WHEN("Remove key 1 with all values") {
+            multi_hash::remove_all(hash_map, 1);
+
+            THEN( "key 1 does not have values" ) {
+                REQUIRE(multi_hash::count(hash_map, 1) == 0);
+            }
+        }
+    }
+
+    memory_globals::shutdown();
+}
+
 #endif
