326d0759331f501ee05522adae2f98a7c8d8dda7
Ondra Voves <o.voves@gmail.com>
2015-12-06 02:14:35 +0100

    add color and template to log_view.p

--- log_view.py
+++ log_view.py
@@ -10,12 +10,22 @@
 import re
 import sys
 import argparse
+from argparse import RawTextHelpFormatter
 
 import yaml
 
 ###########
 # GLOBALS #
 ########################################################################################################################
+TEMPLATE_ARGS = {
+    'level': 'Log level',
+    'where': 'Where',
+    'time': 'Time',
+    'worker': 'Worker ID',
+    'msg': 'Message'
+}
+
+TEMPLATE_ARGS_HELP = '\n\t'.join(["{%s} = %s" % (k, v) for k, v in TEMPLATE_ARGS.items()])
 
 PRINT_TEMPLATE = "---\n" \
                  "level: {level}\n" \
@@ -29,12 +39,24 @@ PRINT_TEMPLATE = "---\n" \
 # CONFIG #
 ########################################################################################################################
 
+COLOR_RED = "\x1B[31m"
+COLOR_GREEN = "\x1B[32m"
+COLOR_YELLOW = "\x1B[33m"
+COLOR_BLUE = "\x1B[34m"
+COLOR_RESET = "\033[0m"
+
+LEVEL_TO_COLOR = {
+    'info': COLOR_BLUE + "%s" + COLOR_RESET,
+    'error': COLOR_RED + "%s" + COLOR_RESET,
+    'debug': COLOR_GREEN + "%s" + COLOR_RESET,
+    'warning': COLOR_YELLOW + "%s" + COLOR_RESET,
+}
 
 ########
 # ARGS #
 ########################################################################################################################
 
-ARGS_PARSER = argparse.ArgumentParser(description='CETech yaml log view script')
+ARGS_PARSER = argparse.ArgumentParser(description='CETech yaml log view script', formatter_class=RawTextHelpFormatter)
 
 ARGS_PARSER.add_argument(
         "-i", "--info",
@@ -67,11 +89,10 @@ ARGS_PARSER.add_argument(
         type=str)
 
 ARGS_PARSER.add_argument(
-        "logfile",
-        nargs='?',
-        help='Log file',
-        default='log.yaml',
-        type=argparse.FileType('r'))
+        "-t", "--template",
+        help='Output template. Variables:\n\t%s' % TEMPLATE_ARGS_HELP,
+        default=PRINT_TEMPLATE,
+        type=str)
 
 ARGS_PARSER.add_argument(
         "-o", "--out",
@@ -79,6 +100,18 @@ ARGS_PARSER.add_argument(
         help='Output file',
         default=sys.stdout,
         type=argparse.FileType('w'))
+
+ARGS_PARSER.add_argument(
+        "logfile",
+        nargs='?',
+        help='Log file. \'-\' for stdin',
+        default='log.yaml',
+        type=argparse.FileType('r'))
+
+ARGS_PARSER.add_argument(
+        "--no-color",
+        help='Disable colors',
+        action='store_true')
 
 
 ###########
@@ -89,6 +122,10 @@ def main(args=None):
     """
 
     args = ARGS_PARSER.parse_args(args=args)
+
+    if args.no_color:
+        global LEVEL_TO_COLOR
+        LEVEL_TO_COLOR = {k: "%s" for k, v in LEVEL_TO_COLOR.items()}
 
     if not (args.info or args.error or args.debug or args.warn):
         level_mask = {"info", "debug", "warning", "error"}
@@ -113,7 +150,7 @@ def main(args=None):
         where_re = None
 
     if args.msg:
-        msg_re = re.compile(args.msg)
+        msg_re = re.compile(args.msg, re.MULTILINE)
     else:
         msg_re = None
 
@@ -121,7 +158,8 @@ def main(args=None):
 
     out = args.out
     for entry in log_yaml:
-        if entry['level'] not in level_mask:
+        level = entry['level']
+        if level not in level_mask:
             continue
 
         if where_re and not where_re.match(entry['where']):
@@ -130,7 +168,7 @@ def main(args=None):
         if msg_re and not msg_re.match(entry['msg']):
             continue
 
-        out.write(PRINT_TEMPLATE.format(**entry))
+        out.write(LEVEL_TO_COLOR[level] % args.template.format(**entry).replace('\\n', '\n'))
 
 
 ########
