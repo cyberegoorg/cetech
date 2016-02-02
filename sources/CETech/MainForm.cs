bb809b97397e91fc248e727085b7e9c0de9f6733
Ondra Voves <o.voves@gmail.com>
2016-02-02 22:23:49 +0100

    LogSysten in sharp

---
+++ sources/CETech/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using CELib;
+
+namespace CETech
+{
+    public partial class MainForm : Form
+    {
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            LogHandler.FileLog file_log = new LogHandler.FileLog("log.md");
+
+            Log.LogEvent += LogHandler.console_log;
+            Log.LogEvent += file_log.Log;
+
+
+            Log.Info("dddd", "dsasadsadsad");
+            Log.Info("dddd", "dsasadsadsad");
+            Log.Info("dddd", "dsasadsadsad");
+        }
+    }
+}
