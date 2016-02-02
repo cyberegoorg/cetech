f11caa215212123a28d6d91d64539f07de01eae6
Ondra Voves <o.voves@gmail.com>
2016-02-02 22:24:42 +0100

    Add celib

---
+++ CELib/Log.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CELib
+{
+    /// <summary>
+    /// Log system.
+    /// </summary>
+    public static class Log
+    {
+        /// <summary>
+        /// Log level enum.
+        /// </summary>
+        public enum Level
+        {
+            Info,
+            Warning,
+            Error,
+            Debug
+        }
+
+        public delegate void LogHandler(Level level, DateTime time, int workerId, string where, string msg, params object[] args);
+        public static event LogHandler LogEvent;
+
+        public static void Info(string where, string msg, params object[] args)
+        {
+            OnLog(Level.Info, DateTime.Now, 0, where, msg, args);
+        }
+
+        public static void Warning(string where, string msg, params object[] args)
+        {
+            OnLog(Level.Warning, DateTime.Now, 0, where, msg, args);
+        }
+
+
+        public static void Error(string where, string msg, params object[] args)
+        {
+            OnLog(Level.Error, DateTime.Now, 0, where, msg, args);
+        }
+
+        public static void Debug(string where, string msg, params object[] args)
+        {
+#if DEBUG
+            OnLog(Level.Debug, DateTime.Now, 0, where, msg, args);
+#endif
+        }
+
+
+        private static void OnLog(Level level, DateTime time, int workerid, string @where, string msg, params object[] args)
+        {
+            var handler = LogEvent;
+            if (handler != null) handler(level, time, workerid, @where, msg, args);
+        }
+
+    }
+}
---
+++ CELib/LogHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace CELib
+{
+    public class LogHandler
+    {
+        private static readonly string LogFormat = "---" + System.Environment.NewLine +
+                                                   "level: {0}" + System.Environment.NewLine +
+                                                   "where: {1}" + System.Environment.NewLine +
+                                                   "time: {2}" + System.Environment.NewLine +
+                                                   "worker: {3}" + System.Environment.NewLine +
+                                                   "msg: |" + System.Environment.NewLine +
+                                                   "  {4}\n" + System.Environment.NewLine;
+
+        internal static ConsoleColor LevelToColor(Log.Level level)
+        {
+            switch (level)
+            {
+                case Log.Level.Info:
+                    return ConsoleColor.Blue;
+                case Log.Level.Warning:
+                    return ConsoleColor.Yellow;
+                case Log.Level.Error:
+                    return ConsoleColor.Red;
+                case Log.Level.Debug:
+                    return ConsoleColor.Green;
+                default:
+                    throw new ArgumentOutOfRangeException("level", level, null);
+            }
+        }
+
+        public static void console_log(Log.Level level, DateTime time, int workerId, string where, string msg, params Object[] args)
+        {
+            Console.ForegroundColor = LevelToColor(level);
+            Console.WriteLine(LogFormat, level, where, time, workerId, String.Format(msg, args));
+            Console.ResetColor();
+        }
+
+
+        public class FileLog : LogHandler
+        {
+            private readonly StreamWriter _write;
+
+            public FileLog(string filename)
+            {
+                _write = new StreamWriter(filename);
+            }
+
+            public void Log(Log.Level level, DateTime time, int workerId, string where, string msg, params Object[] args)
+            {
+                _write.Write(LogFormat, level, where, time, workerId, String.Format(msg, args));
+                _write.Flush();
+            }
+        }
+    }
+}
