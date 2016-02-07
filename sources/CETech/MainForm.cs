9a322d050b4bb981e59eb84db5134f311c433e34
Ondra Voves <o.voves@gmail.com>
2016-02-07 20:11:09 +0100

    cleanup

--- sources/CETech/MainForm.cs
+++ sources/CETech/MainForm.cs
@@ -6,12 +6,11 @@ namespace CETech
 {
     public partial class MainForm : Form
     {
-
         public MainForm()
         {
             InitializeComponent();
 
-            LogHandler.FileLog file_log = new LogHandler.FileLog("log.md");
+            var file_log = new LogHandler.FileLog("log.md");
 
             Log.LogEvent += LogHandler.console_log;
             Log.LogEvent += file_log.Log;
@@ -23,9 +22,11 @@ namespace CETech
             Log.Debug("MainForm", "Debug");
         }
 
-        private void ListLog(Log.Level level, DateTime time, int workerId, string where, string msg, params object[] args)
+        private void ListLog(Log.Level level, DateTime time, int workerId, string where, string msg,
+            params object[] args)
         {
-            listBox1.Items.Add(string.Format(LogHandler.LogFormat, level, where, time, workerId, string.Format(msg, args)));
+            listBox1.Items.Add(string.Format(LogHandler.LogFormat, level, where, time, workerId,
+                string.Format(msg, args)));
         }
 
         private void button1_Click(object sender, EventArgs e)
