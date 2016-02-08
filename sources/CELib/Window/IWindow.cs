cb56ce7e531f597df9baa5d413e7eeee116fb290
Ondra Voves <o.voves@gmail.com>
2016-02-08 20:42:25 +0100

    Try 1

--- sources/CELib/Window/IWindow.cs
+++ sources/CELib/Window/IWindow.cs
@@ -1,8 +1,13 @@
-﻿namespace CELib.Window
+﻿using System;
+
+namespace CELib.Window
 {
     public interface IWindow
     {
         string Title { get; set; }
+        int Width { get; }
+        int Height { get; }
+        IntPtr NativeWindowPtr { get; }
 
         void Update();
         void Resize(int width, int height);
