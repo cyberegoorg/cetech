2642ae57e49cea5ca35114d25c82d92a8dbef24b
Ondra Voves <o.voves@gmail.com>
2016-02-08 20:48:09 +0100

    move window to cetech

---
+++ sources/CETech/Window/IWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CELib.Window
+{
+    public interface IWindow
+    {
+        string Title { get; set; }
+        int Width { get; }
+        int Height { get; }
+        IntPtr NativeWindowPtr { get; }
+
+        void Update();
+        void Resize(int width, int height);
+    }
+}
---
+++ sources/CETech/Window/Window.cs
@@ -0,0 +1,17 @@
+﻿using CELib.Window.Private;
+
+namespace CELib.Window
+{
+    public static class Window
+    {
+        public static IWindow CreateWindow(string title, WindowPos x, WindowPos y, int width, int height, int flags)
+        {
+
+#if PLATFORM_SDL2
+            return new SDLWindow(title, x, y, width, height, flags);
+#else
+#error Invalid platform
+#endif
+        }
+    }
+}
