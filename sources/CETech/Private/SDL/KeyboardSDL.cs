244b298d506386b8831491b658f04eb63952f562
Ondra Voves <o.voves@gmail.com>
2016-02-09 21:01:45 +0100

    ...

--- sources/CETech/Private/SDL/KeyboardSDL.cs
+++ sources/CETech/Private/SDL/KeyboardSDL.cs
@@ -10,11 +10,11 @@ namespace CETech.Input
         private static readonly byte[] _keyboardStates = new byte[512];
         private static readonly byte[] _keyboardStatesLast = new byte[512];
 
-        public static void PlatformInit()
+        private static void PlatformInit()
         {
         }
 
-        public static void PlatformProcessKeyboard()
+        private static void PlatformProcessKeyboard()
         {
             int numKeys;
 
@@ -22,27 +22,27 @@ namespace CETech.Input
             Marshal.Copy(SDL.SDL_GetKeyboardState(out numKeys), _keyboardStates, 0, 512);
         }
 
-        public static int PlatformButtonIndex(string buttonName)
+        private static int PlatformButtonIndex(string buttonName)
         {
             return (int) SDL.SDL_GetScancodeFromName(buttonName);
         }
 
-        public static string PlatformButtonName(int buttonIndex)
+        private static string PlatformButtonName(int buttonIndex)
         {
             return SDL.SDL_GetScancodeName((SDL.SDL_Scancode) buttonIndex);
         }
 
-        public static bool PlatformButtonState(int buttonIndex)
+        private static bool PlatformButtonState(int buttonIndex)
         {
             return _keyboardStates[buttonIndex] != 0;
         }
 
-        public static bool PlatformButtonPressed(int buttonIndex)
+        private static bool PlatformButtonPressed(int buttonIndex)
         {
             return _keyboardStates[buttonIndex] == 1 && _keyboardStatesLast[buttonIndex] == 0;
         }
 
-        public static bool PlatformButtonReleased(int buttonIndex)
+        private static bool PlatformButtonReleased(int buttonIndex)
         {
             return _keyboardStates[buttonIndex] == 0 && _keyboardStatesLast[buttonIndex] == 1;
         }
--- sources/CETech/Private/SDL/MouseSDL.cs
+++ sources/CETech/Private/SDL/MouseSDL.cs
@@ -11,17 +11,17 @@ namespace CETech.Input
         private static int _posX;
         private static int _posY;
 
-        public static void PlatformInit()
+        private static void PlatformInit()
         {
         }
 
-        public static void PlatformProcess()
+        private static void PlatformProcess()
         {
             _buttonStatesLast = _buttonStates;
             _buttonStates = (int) SDL.SDL_GetMouseState(out _posX, out _posY);
         }
 
-        public static int PlatformButtonIndex(string buttonName)
+        private static int PlatformButtonIndex(string buttonName)
         {
             switch (buttonName)
             {
@@ -36,7 +36,7 @@ namespace CETech.Input
             return 0;
         }
 
-        public static string PlatformButtonName(int buttonIndex)
+        private static string PlatformButtonName(int buttonIndex)
         {
             if (buttonIndex == SDL.SDL_BUTTON_LMASK)
             {
@@ -51,17 +51,17 @@ namespace CETech.Input
             return buttonIndex == SDL.SDL_BUTTON_RMASK ? "right" : "";
         }
 
-        public static bool PlatformButtonState(int buttonIndex)
+        private static bool PlatformButtonState(int buttonIndex)
         {
             return (_buttonStates & buttonIndex) > 0;
         }
 
-        public static bool PlatformButtonPressed(int buttonIndex)
+        private static bool PlatformButtonPressed(int buttonIndex)
         {
             return (_buttonStates & buttonIndex) == 1 && (_buttonStatesLast & buttonIndex) == 0;
         }
 
-        public static bool PlatformButtonReleased(int buttonIndex)
+        private static bool PlatformButtonReleased(int buttonIndex)
         {
             return (_buttonStates & buttonIndex) == 0 && (_buttonStatesLast & buttonIndex) == 1;
         }
