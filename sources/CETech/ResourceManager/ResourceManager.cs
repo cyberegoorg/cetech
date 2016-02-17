60effe015f1a07924a75ef859733e728a09500a9
Ondra Voves <o.voves@gmail.com>
2016-02-17 22:35:12 +0100

    ...

--- sources/CETech/ResourceManager/ResourceManager.cs
+++ sources/CETech/ResourceManager/ResourceManager.cs
@@ -34,24 +34,24 @@ namespace CETech
         /// <param name="data">Resource data</param>
         public delegate void ResourceUnloader(object data);
 
-        private static readonly Dictionary<long, ResourceLoader> _loader_map =
+        private static readonly Dictionary<long, ResourceLoader> LoaderMap =
             new Dictionary<long, ResourceLoader>();
 
-        private static readonly Dictionary<long, ResourceUnloader> _unloader_map =
+        private static readonly Dictionary<long, ResourceUnloader> UnloaderMap =
             new Dictionary<long, ResourceUnloader>();
 
-        private static readonly Dictionary<long, ResourceOnline> _online_map =
+        private static readonly Dictionary<long, ResourceOnline> OnlineMap =
             new Dictionary<long, ResourceOnline>();
 
-        private static readonly Dictionary<long, ResourceOffline> _offline_map =
+        private static readonly Dictionary<long, ResourceOffline> OfflineMap =
             new Dictionary<long, ResourceOffline>();
 
-        private static readonly Dictionary<long, int> _types_map = new Dictionary<long, int>();
+        private static readonly Dictionary<long, int> TypesMap = new Dictionary<long, int>();
 
-        private static readonly List<Dictionary<long, object>> _data_map =
+        private static readonly List<Dictionary<long, object>> DataMap =
             new List<Dictionary<long, object>>();
 
-        private static readonly List<Dictionary<long, int>> _ref_map = new List<Dictionary<long, int>>();
+        private static readonly List<Dictionary<long, int>> RefMap = new List<Dictionary<long, int>>();
 
         private static SpinLock _addLock = new SpinLock();
 
@@ -71,16 +71,16 @@ namespace CETech
         public static void RegisterType(long type, ResourceLoader loader, ResourceUnloader unloader,
             ResourceOnline online, ResourceOffline offline)
         {
-            var idx = _data_map.Count;
-            _data_map.Add(new Dictionary<long, object>());
-            _ref_map.Add(new Dictionary<long, int>());
+            var idx = DataMap.Count;
+            DataMap.Add(new Dictionary<long, object>());
+            RefMap.Add(new Dictionary<long, int>());
 
-            _types_map[type] = idx;
+            TypesMap[type] = idx;
 
-            _loader_map[type] = loader;
-            _unloader_map[type] = unloader;
-            _online_map[type] = online;
-            _offline_map[type] = offline;
+            LoaderMap[type] = loader;
+            UnloaderMap[type] = unloader;
+            OnlineMap[type] = online;
+            OfflineMap[type] = offline;
         }
 
         /// <summary>
@@ -99,7 +99,7 @@ namespace CETech
 
                 var input = FileSystem.Open("build", string.Format("{0:x}{1:x}", type, names[i]),
                     FileSystem.OpenMode.Read);
-                data[i] = _loader_map[type](input);
+                data[i] = LoaderMap[type](input);
             }
 
             return data;
@@ -113,8 +113,8 @@ namespace CETech
         /// <param name="names">Resource names</param>
         public static void AddLoaded(object[] loaded_data, long type, long[] names)
         {
-            var online = _online_map[type];
-            var idx = _types_map[type];
+            var online = OnlineMap[type];
+            var idx = TypesMap[type];
 
             var gotLock = false;
             try
@@ -125,9 +125,9 @@ namespace CETech
                 {
                     IncRef(idx, names[i]);
 
-                    _data_map[idx][names[i]] = loaded_data[i];
+                    DataMap[idx][names[i]] = loaded_data[i];
 
-                    online(_data_map[idx][names[i]]);
+                    online(DataMap[idx][names[i]]);
                 }
             }
             finally
@@ -155,24 +155,24 @@ namespace CETech
         /// <param name="names">Resource names</param>
         public static void Unload(long type, long[] names)
         {
-            var offline = _offline_map[type];
-            var unloader = _unloader_map[type];
+            var offline = OfflineMap[type];
+            var unloader = UnloaderMap[type];
 
-            var idx = _types_map[type];
+            var idx = TypesMap[type];
 
             for (var i = 0; i < names.Length; i++)
             {
-                offline(_data_map[idx][names[i]]);
+                offline(DataMap[idx][names[i]]);
             }
 
             for (var i = 0; i < names.Length; i++)
             {
                 if (DecRef(idx, names[i]))
                 {
-                    unloader(_data_map[idx][names[i]]);
+                    unloader(DataMap[idx][names[i]]);
 
-                    _data_map[idx].Remove(names[i]);
-                    _ref_map[idx].Remove(names[i]);
+                    DataMap[idx].Remove(names[i]);
+                    RefMap[idx].Remove(names[i]);
                 }
             }
         }
@@ -185,10 +185,10 @@ namespace CETech
         /// <returns>True if can</returns>
         public static bool CanGet(long type, long[] names)
         {
-            var idx = _types_map[type];
+            var idx = TypesMap[type];
             for (var i = 0; i < names.Length; i++)
             {
-                if (!_data_map[idx].ContainsKey(names[i]))
+                if (!DataMap[idx].ContainsKey(names[i]))
                 {
                     return false;
                 }
@@ -213,27 +213,27 @@ namespace CETech
                 LoadNow(type, names);
             }
 
-            var idx = _types_map[type];
-            return (T) _data_map[idx][name];
+            var idx = TypesMap[type];
+            return (T) DataMap[idx][name];
         }
 
         private static void IncRef(int type_idx, long name)
         {
             int counter;
 
-            if (!_ref_map[type_idx].TryGetValue(name, out counter))
+            if (!RefMap[type_idx].TryGetValue(name, out counter))
             {
                 counter = 0;
             }
 
-            _ref_map[type_idx][name] = counter + 1;
+            RefMap[type_idx][name] = counter + 1;
         }
 
         private static bool DecRef(int type_idx, long name)
         {
-            _ref_map[type_idx][name] -= 1;
+            RefMap[type_idx][name] -= 1;
 
-            return _ref_map[type_idx][name] == 0;
+            return RefMap[type_idx][name] == 0;
         }
     }
 }
