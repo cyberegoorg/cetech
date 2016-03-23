f3e71131d9f20d27c824f2fb0136aed9d1d4a4fa
Ondra Voves <o.voves@gmail.com>
2016-03-23 07:04:17 +0100

    Fix linux build

--- sources/CETech/Utils/Options.cs
+++ sources/CETech/Utils/Options.cs
@@ -234,7 +234,7 @@ namespace Mono.Options
 
         private static int GetLineEnd(int start, int length, string description)
         {
-            var end = Math.Min(start + length, description.Length);
+            var end = System.Math.Min(start + length, description.Length);
             var sep = -1;
 
             for (var i = start; i < end; ++i)
