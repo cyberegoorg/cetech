9ed9488d7c565bd47cf45c2078307525045bdc29
Ondra Voves <o.voves@gmail.com>
2016-03-25 00:09:27 +0100

    lua api

---
+++ sources/CETech/Lua/Private/Api/ApplicationApi.cs
@@ -0,0 +1,13 @@
+using MoonSharp.Interpreter;
+
+namespace CETech.Lua.Api
+{
+    [MoonSharpUserData]
+    internal class ApplicationApi
+    {
+        public static void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
