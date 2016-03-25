abc5989502bb2f9fdce6c5b7709d9db2120dfc06
Ondra Voves <o.voves@gmail.com>
2016-03-25 17:06:09 +0100

    EntityManager (fix #161)

---
+++ sources/CETech/EntityManager/EntityManager.cs
@@ -0,0 +1,54 @@
+﻿// ReSharper disable once CheckNamespace
+namespace CETech
+{
+
+    /// <summary>
+    /// Entity manager
+    /// </summary>
+    public partial class EntityManager
+    {
+        /// <summary>
+        /// Init entity manager
+        /// </summary>
+        public static void Init()
+        {
+            InitImpl();
+        }
+
+        /// <summary>
+        /// Shutdown entity manager.
+        /// </summary>
+        public static void Shutdown()
+        {
+            ShutdownImpl();
+        }
+
+        /// <summary>
+        /// Create new entity
+        /// </summary>
+        /// <returns>entity handler</returns>
+        public static int Create()
+        {
+            return CreateImpl();
+        }
+
+        /// <summary>
+        /// Destroy entity.
+        /// </summary>
+        /// <param name="entity">entity handler</param>
+        public static void Destroy(int entity)
+        {
+            DestroyImpl(entity);
+        }
+
+        /// <summary>
+        /// Is entity alive?
+        /// </summary>
+        /// <param name="entity">Entity handler</param>
+        /// <returns>True if entity is live else return false</returns>
+        public static bool Alive(int entity)
+        {
+            return AliveImpl(entity);
+        }
+    }
+}
