7412bac056afcfb9e81b30d19b142e637fe4a4d8
Ondra Voves <o.voves@gmail.com>
2016-03-25 22:23:08 +0100

    nspec

---
+++ sources/CETech/EntityManager/Tests/EntityManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NSpec;
+
+namespace CETech.Tests
+{
+    public class EntityManagerTest: nspec
+    {
+
+
+        void before_each() { Console.WriteLine("before"); name = "NSpec"; }
+
+        void after_each() { Console.WriteLine("after"); }
+
+        void it_works()
+        {
+            name.should_be("NSpec");
+        }
+
+        void describe_nesting()
+        {
+            before = () => name += " BDD";
+
+            it["works here"] = () => name.should_be("NSpec BDD");
+
+            it["and here"] = () => name.should_be("NSpec BDD");
+        }
+        string name;
+    }
+}
