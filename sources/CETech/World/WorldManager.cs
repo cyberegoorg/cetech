808c41dc665f755295927143dc076fc5ff9658df
Ondra Voves <o.voves@gmail.com>
2016-03-28 22:22:43 +0200

    world manager draft

---
+++ sources/CETech/World/WorldManager.cs
@@ -0,0 +1,25 @@
+﻿namespace CETech.World
+{
+    public partial class WorldManager
+    {
+        public static void Init()
+        {
+            InitImpl();
+        }
+
+        public static void Shutdown()
+        {
+            ShutdownImpl();
+        }
+
+        public static int CreateWorld()
+        {
+            return CreateWorldImpl();
+        }
+
+        public static void DestroyWorld(int world)
+        {
+            DestroyWorldImpl(world);
+        }
+    }
+}
