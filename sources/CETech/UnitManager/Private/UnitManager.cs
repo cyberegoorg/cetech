b7b669ad923fbe4ba30436647f71655d9d036e13
Ondra Voves <o.voves@gmail.com>
2016-03-29 21:49:21 +0200

    change unit format

--- sources/CETech/UnitManager/Private/UnitManager.cs
+++ sources/CETech/UnitManager/Private/UnitManager.cs
@@ -11,17 +11,35 @@ namespace CETech
             var unit_resource = ResourceManager.Get<Dictionary<MessagePackObject, MessagePackObject>>(
                 UnitResource.Type, unit);
 
-            var entity = EntityManager.Create();
-
             var types = unit_resource["type"].AsList();
             var data = unit_resource["data"].AsList();
+            var ent_list = unit_resource["ent"].AsList();
+            var ent_count = unit_resource["ent_count"].AsInt32();
+
+
+            var entities = new int[ent_count];
+            for (int i = 0; i < ent_count; i++)
+            {
+                entities[i] = EntityManager.Create();
+            }
 
             for (var i = 0; i < types.Count; ++i)
             {
-                ComponentSystem.Spawn(world, types[i].AsInt64(), new[] {entity}, new[] {data[i].AsDictionary()});
+                var ents = ent_list[i].AsList();
+                var edata = data[i].AsList();
+
+                var entities_id = new int[ents.Count];
+                var entities_body = new MessagePackObjectDictionary[ents.Count];
+                for (int j = 0; j < ents.Count; j++)
+                {
+                    entities_id[j] = entities[ents[j].AsInt32()];
+                    entities_body[j] = edata[j].AsDictionary();
+                }
+
+                ComponentSystem.Spawn(world, types[i].AsInt64(), entities_id, entities_body);
             }
 
-            return entity;
+            return entities[0];
         }
 
         private static void InitImpl()
--- sources/CETech/UnitManager/UnitResource.cs
+++ sources/CETech/UnitManager/UnitResource.cs
@@ -16,6 +16,46 @@ namespace CETech.World
 
 #if CETECH_DEVELOP
 
+        private static void compile_entitity(YamlMappingNode rootNode, ref int entities_id, List<long> components_type,
+            Dictionary<long, List<long>> component_ent, Dictionary<long, List<YamlMappingNode>> components_body)
+        {
+            var componentsNode = rootNode.Children[new YamlScalarNode("components")] as YamlMappingNode;
+            foreach (var component in componentsNode.Children)
+            {
+                var components_id = component.Key as YamlScalarNode;
+                var component_body = component.Value as YamlMappingNode;
+                var component_type = component_body.Children[new YamlScalarNode("component_type")] as YamlScalarNode;
+
+                var cid = StringId.FromString(component_type.Value);
+                if (!components_type.Contains(cid))
+                {
+                    components_type.Add(cid);
+                    component_ent[cid] = new List<long>();
+                }
+
+                if (!components_body.ContainsKey(cid))
+                {
+                    components_body[cid] = new List<YamlMappingNode>();
+                }
+
+
+                component_ent[cid].Add(entities_id);
+                components_body[cid].Add(component_body);
+            }
+
+
+            if (rootNode.Children.ContainsKey(new YamlScalarNode("children")))
+            {
+                var childrenNode = rootNode.Children[new YamlScalarNode("children")] as YamlMappingNode;
+                foreach (var child in childrenNode.Children)
+                {
+                    entities_id += 1;
+                    compile_entitity(child.Value as YamlMappingNode, ref entities_id, components_type, component_ent,
+                        components_body);
+                }
+            }
+        }
+
         /// <summary>
         ///     Resource compiler
         /// </summary>
@@ -27,25 +67,21 @@ namespace CETech.World
             yaml.Load(input);
 
             var rootNode = yaml.Documents[0].RootNode as YamlMappingNode;
-            var componentsNode = rootNode.Children[new YamlScalarNode("components")] as YamlMappingNode;
+
+            int entities_id = 0;
 
             var components_type = new List<long>();
-            var components_body = new List<YamlMappingNode>();
+            var component_ent = new Dictionary<long, List<long>>();
+            var components_body = new Dictionary<long, List<YamlMappingNode>>();
 
-            foreach (var component in componentsNode.Children)
-            {
-                var components_id = component.Key as YamlScalarNode;
-
-                var component_body = component.Value as YamlMappingNode;
-                components_body.Add(component_body);
-
-                var component_type = component_body.Children[new YamlScalarNode("component_type")] as YamlScalarNode;
-                components_type.Add(StringId.FromString(component_type.Value));
-            }
+            compile_entitity(rootNode, ref entities_id, components_type, component_ent, components_body);
 
             var packer = new ConsoleServer.ResponsePacker();
 
-            packer.PackMapHeader(2);
+            packer.PackMapHeader(4);
+
+            packer.Pack("ent_count");
+            packer.Pack(entities_id + 1);
 
             packer.Pack("type");
             packer.PackArrayHeader(components_type.Count);
@@ -54,12 +90,35 @@ namespace CETech.World
                 packer.Pack(components_type[i]);
             }
 
-            packer.Pack("data");
-            packer.PackArrayHeader(components_body.Count);
-            for (var i = 0; i < components_body.Count; ++i)
+            packer.Pack("ent");
+            packer.PackArrayHeader(components_type.Count);
+            for (var i = 0; i < components_type.Count; ++i)
             {
-                ComponentSystem.Compile(components_type[i], components_body[i], packer);
+                var comp_type = components_type[i];
+                var ents = component_ent[comp_type];
+
+                packer.PackArrayHeader(ents.Count);
+                for (int j = 0; j < ents.Count; j++)
+                {
+                    packer.Pack(ents[j]);
+                }
+           }
+
+            packer.Pack("data");
+            packer.PackArrayHeader(components_type.Count);
+            for (var i = 0; i < components_type.Count; ++i)
+            {
+                var comp_type = components_type[i];
+                var comp_body = components_body[comp_type];
+
+                packer.PackArrayHeader(comp_body.Count);
+                for (int j = 0; j < comp_body.Count; j++)
+                {
+                    ComponentSystem.Compile(comp_type, comp_body[j], packer);
+                }
             }
+
+            //
 
             packer.GetMemoryStream().WriteTo(capi.BuildFile);
         }
