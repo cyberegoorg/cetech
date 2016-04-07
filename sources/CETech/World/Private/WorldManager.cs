160fb978cf3ade580009ad695829fb7b1bef4a33
Ondra Voves <o.voves@gmail.com>
2016-04-07 19:45:37 +0200

    world destroy (fix #168)

--- sources/CETech/World/Private/WorldManager.cs
+++ sources/CETech/World/Private/WorldManager.cs
@@ -10,13 +10,22 @@ namespace CETech.World
     {
         private static HandlerID _handlers;
 
-        //TODO: arrays
-        private static readonly Dictionary<long, List<Levelnstance>> _levelnstances =
-            new Dictionary<long, List<Levelnstance>>();
+        private static Dictionary<int, int> _WorldIdx;
+
+        private static List<int> _WordlId;
+        private static List<List<int>> _SpawnedUnits;
+        private static List<Levelnstance> _levelnstances;
+
 
         public static void InitImpl()
         {
             _handlers = new HandlerID();
+
+            _WorldIdx = new Dictionary<int, int>();
+            _SpawnedUnits = new List<List<int>>();
+            _WordlId = new List<int>();
+            _levelnstances = new List<Levelnstance>();
+
             Debug.Assert(_handlers.Create() == NullWorld);
         }
 
@@ -27,11 +36,24 @@ namespace CETech.World
         public static int CreateWorldImpl()
         {
             var id = _handlers.Create();
+
+            var idx = _WorldIdx.Count;
+
+            _WorldIdx[id] = idx;
+            _SpawnedUnits.Add(new List<int>());
+            _WordlId.Add(id);
+            _levelnstances.Add(new Levelnstance());
+
             TranformationSystem.InitWorld(id);
             PrimitiveMeshRenderer.InitWorld(id);
             CameraSystem.InitWorld(id);
-            _levelnstances[id] = new List<Levelnstance>();
+
             return id;
+        }
+
+        private static int getIdx(int world)
+        {
+            return _WorldIdx[world];
         }
 
         private static void DestroyWorldImpl(int world)
@@ -39,6 +61,23 @@ namespace CETech.World
             TranformationSystem.RemoveWorld(world);
             PrimitiveMeshRenderer.RemoveWorld(world);
             CameraSystem.RemoveWorld(world);
+
+            var item_idx = getIdx(world);
+            var last_idx = _WorldIdx.Count - 1;
+            var last_id = _WordlId[last_idx];
+
+            _WorldIdx.Remove(world);
+
+            // Move value from last to actual
+            _SpawnedUnits[item_idx] = _SpawnedUnits[last_idx];
+            _WordlId[item_idx] = _WordlId[last_idx];
+
+            // Remove last
+            _SpawnedUnits.RemoveAt(last_idx);
+            _WordlId.RemoveAt(last_idx);
+
+            _WorldIdx[last_id] = item_idx;
+
             _handlers.Destroy(world);
         }
 
@@ -49,8 +88,12 @@ namespace CETech.World
 
         private static int LoadLevelImpl(int world, long level, Vector3f position, Vector3f rotation, Vector3f scale)
         {
-            var level_resource = ResourceManager.Get<LevelResource.CompiledResource>(LevelResource.Type, level);
+            var world_idx = getIdx(world);
+
             var level_instance = new Levelnstance();
+
+            var level_resource = ResourceManager.Get<LevelResource.CompiledResource>(LevelResource.Type, level);
+
 
             var units = level_resource.units;
             var units_name = level_resource.units_name;
@@ -63,20 +106,22 @@ namespace CETech.World
             for (var i = 0; i < units.Length; ++i)
             {
                 var spawned_unit = UnitManager.Spawn(units[i], world);
-                
+
                 level_instance.units[units_name[i]] = spawned_unit;
 
                 TranformationSystem.Link(world, level_ent, spawned_unit);
             }
 
-            _levelnstances[world].Add(level_instance);
+            _levelnstances[world_idx] = level_instance;
 
-            return _levelnstances[world].Count - 1; // TODO
+            return _levelnstances.Count - 1; // TODO
         }
 
         private static int UnitByNameImpl(int world, int level, long name)
         {
-            var level_instance = _levelnstances[world][level];
+            var idx = getIdx(world);
+
+            var level_instance = _levelnstances[idx];
 
             int unit;
             return level_instance.units.TryGetValue(name, out unit) ? unit : EntityManager.NullEntity;
@@ -84,7 +129,8 @@ namespace CETech.World
 
         private static int LevelUnitImpl(int world, int level)
         {
-            return _levelnstances[world][level].unit;
+            var idx = getIdx(world);
+            return _levelnstances[idx].unit;
         }
 
         private class Levelnstance
