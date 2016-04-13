8abda1969b89166b379f726bf21acd031dc9258a
Ondra Voves <o.voves@gmail.com>
2016-04-13 19:23:28 +0200

    add game class for non-lua game logic

---
+++ sources/CETech/Game/Private/Game.cs
@@ -0,0 +1,28 @@
+﻿using CETech.Lua;
+
+namespace CETech
+{
+    public partial class Game
+    {
+
+        private static void InitImpl()
+        {
+            LuaEnviroment.BootScriptCallInit();
+        }
+
+        private static void ShutdownImpl()
+        {
+            LuaEnviroment.BootScriptCallShutdown();
+        }
+
+        private static void UpdateImpl(float dt)
+        {
+            LuaEnviroment.BootScriptCallUpdate(dt);
+        }
+
+        private static void RenderImpl()
+        {
+            LuaEnviroment.BootScriptCallRender();
+        }
+    }
+}
