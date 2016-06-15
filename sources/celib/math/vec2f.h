edf5c785e946a31696ebd2bbbaa4f70f32baf821
Ondra Voves <o.voves@gmail.com>
2016-06-15 20:35:39 +0200

    vec2f, vec3f, vec4f

--- sources/celib/math/vec2f.h
+++ sources/celib/math/vec2f.h
@@ -3,44 +3,44 @@
 
 #include <math.h>
 
-static inline void vec2f_set(float* restrict result, const float* restrict a) {
+static inline void vec2f_set(float* __restrict result, const float* __restrict a) {
     result[0] = a[0];
     result[1] = a[1];
 }
 
-static inline void vec2f_add(float* restrict result, const float* restrict a, const float* restrict b) {
+static inline void vec2f_add(float* __restrict result, const float* __restrict a, const float* __restrict b) {
     result[0] = a[0] + b[0];
     result[1] = a[1] + b[1];
 }
 
-static inline void vec2f_sub(float* restrict result, const float* restrict a, const float* restrict b) {
+static inline void vec2f_sub(float* __restrict result, const float* __restrict a, const float* __restrict b) {
     result[0] = a[0] - b[0];
     result[1] = a[1] - b[1];
 }
 
-static inline void vec2f_mul(float* restrict result, const float* restrict a, const float s) {
+static inline void vec2f_mul(float* __restrict result, const float* __restrict a, const float s) {
     result[0] = a[0] * s;
     result[1] = a[1] * s;
 }
 
-static inline void vec2f_div(float* restrict result, const float* restrict a, const float s) {
+static inline void vec2f_div(float* __restrict result, const float* __restrict a, const float s) {
     result[0] = a[0] / s;
     result[1] = a[1] / s;
 }
 
-static inline float vec2f_dot(const float* restrict a, const float* restrict b) {
+static inline float vec2f_dot(const float* __restrict a, const float* __restrict b) {
     return (a[0] * b[0]) + (a[1] * b[1]);
 }
 
-static inline float vec2f_length_squared(const float* restrict a) {
+static inline float vec2f_length_squared(const float* __restrict a) {
     return (a[0] * a[0]) + (a[1] * a[1]);
 }
 
-static inline float vec2f_length(const float* restrict a) {
+static inline float vec2f_length(const float* __restrict a) {
     return sqrtf(vec2f_length_squared(a));
 }
 
-static inline void vec2f_normalized(float* restrict result, const float* restrict a) {
+static inline void vec2f_normalized(float* __restrict result, const float* __restrict a) {
     const float inv_length = 1.0f/vec2f_length(a);
 
     vec2f_mul(result, a, inv_length);
--- sources/celib/math/vec3f.h
+++ sources/celib/math/vec3f.h
@@ -3,49 +3,49 @@
 
 #include <math.h>
 
-static inline void vec3f_set(float* restrict result, const float* restrict a) {
+static inline void vec3f_set(float* __restrict result, const float* __restrict a) {
     result[0] = a[0];
     result[1] = a[1];
     result[2] = a[2];
 }
 
-static inline void vec3f_add(float* restrict result, const float* restrict a, const float* restrict b) {
+static inline void vec3f_add(float* __restrict result, const float* __restrict a, const float* __restrict b) {
     result[0] = a[0] + b[0];
     result[1] = a[1] + b[1];
     result[2] = a[2] + b[2];
 }
 
-static inline void vec3f_sub(float* restrict result, const float* restrict a, const float* restrict b) {
+static inline void vec3f_sub(float* __restrict result, const float* __restrict a, const float* __restrict b) {
     result[0] = a[0] - b[0];
     result[1] = a[1] - b[1];
     result[2] = a[2] - b[2];
 }
 
-static inline void vec3f_mul(float* restrict result, const float* restrict a, const float s) {
+static inline void vec3f_mul(float* __restrict result, const float* __restrict a, const float s) {
     result[0] = a[0] * s;
     result[1] = a[1] * s;
     result[2] = a[2] * s;
 }
 
-static inline void vec3f_div(float* restrict result, const float* restrict a, const float s) {
+static inline void vec3f_div(float* __restrict result, const float* __restrict a, const float s) {
     result[0] = a[0] / s;
     result[1] = a[1] / s;
     result[2] = a[2] / s;
 }
 
-static inline float vec3f_dot(const float* restrict a, const float* restrict b) {
+static inline float vec3f_dot(const float* __restrict a, const float* __restrict b) {
     return (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]);
 }
 
-static inline float vec3f_length_squared(const float* restrict a) {
+static inline float vec3f_length_squared(const float* __restrict a) {
     return (a[0] * a[0]) + (a[1] * a[1]) + (a[2] * a[2]);
 }
 
-static inline float vec3f_length(const float* restrict a) {
+static inline float vec3f_length(const float* __restrict a) {
     return sqrtf(vec3f_length_squared(a));
 }
 
-static inline void vec3f_normalized(float* restrict result, const float* restrict a) {
+static inline void vec3f_normalized(float* __restrict result, const float* __restrict a) {
     const float inv_length = 1.0f/vec3f_length(a);
 
     vec3f_mul(result, a, inv_length);
