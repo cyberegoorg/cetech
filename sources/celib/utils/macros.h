678c2efed85fe2b4af01b700cc3f2c4d5a83f5e4
Ondra Voves <o.voves@gmail.com>
2016-06-16 21:56:43 +0200

    Forse inline macro

---
+++ sources/celib/utils/macros.h
@@ -0,0 +1,6 @@
+#ifndef CETECH_MACROS_H
+#define CETECH_MACROS_H
+
+#define CE_FORCE_INLINE inline __attribute__((always_inline))
+
+#endif //CETECH_MACROS_H
--- sources/celib/utils/utils.h
+++ sources/celib/utils/utils.h
@@ -1,7 +1,6 @@
 #ifndef CETECH_UTILS_H
 #define CETECH_UTILS_H
 
-
 char *utils_stacktrace(int skip);
 void utils_stacktrace_free(char * st);
 
