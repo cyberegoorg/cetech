60db28a67a6795e41f6495754432b2f333175e43
Ondra Voves <o.voves@gmail.com>
2016-08-31 19:35:03 +0200

    refactor array

--- engine/sources/celib/containers/array.h
+++ engine/sources/celib/containers/array.h
@@ -1,3 +1,8 @@
+//==============================================================================
+// Based on bitsquid foundation.
+// git+web: https://bitbucket.org/bitsquid/foundation
+//==============================================================================
+
 #ifndef CETECH_ARRAY_H
 #define CETECH_ARRAY_H
 
@@ -14,10 +19,10 @@
 // Interface macros
 //==============================================================================
 
-#define ARRAY_T(T)                 struct array_##T
+#define ARRAY_T(N)                 struct array_##N
 
-#define ARRAY_INIT(T, a, alloc)    array_init_##T(a, alloc)
-#define ARRAY_DESTROY(T, a)        array_destroy_##T(a)
+#define ARRAY_INIT(N, a, alloc)    array_init_##N(a, alloc)
+#define ARRAY_DESTROY(N, a)        array_destroy_##N(a)
 
 #define ARRAY_SIZE(a)              (a)->size
 #define ARRAY_CAPACITY(a)          (a)->capacity
@@ -25,27 +30,28 @@
 #define ARRAY_END(a)               ((a)->data + (a)->size)
 #define ARRAY_AT(a, i)             (a)->data[i]
 
-#define ARRAY_RESIZE(T, a, s)       array_resize_##T(a, s)
-#define ARRAY_RESERVE(T, a, s)      array_reserve_##T(a, s)
+#define ARRAY_RESIZE(N, a, s)       array_resize_##N(a, s)
+#define ARRAY_RESERVE(N, a, s)      array_reserve_##N(a, s)
 
-#define ARRAY_PUSH_BACK(T, a, i)   array_push_back_##T(a, i)
-#define ARRAY_PUSH(T, a, i, c)     array_push_##T(a, i, c)
-#define ARRAY_POP_BACK(T, a)       array_pop_back_##T(a)
+#define ARRAY_PUSH_BACK(N, a, i)   array_push_back_##N(a, i)
+#define ARRAY_PUSH(N, a, i, c)     array_push_##N(a, i, c)
+#define ARRAY_POP_BACK(N, a)       array_pop_back_##N(a)
 
 
 //==============================================================================
 // Prototypes macro
 //==============================================================================
 
-#define ARRAY_PROTOTYPE(T)                                                      \
-    struct array_##T {                                                          \
+#define ARRAY_PROTOTYPE(T)  ARRAY_PROTOTYPE_N(T, T)
+#define ARRAY_PROTOTYPE_N(T, N)                                                 \
+    struct array_##N {                                                          \
         struct allocator* allocator;                                            \
         T *data;                                                                \
         size_t size;                                                            \
         size_t capacity;                                                        \
     };                                                                          \
                                                                                 \
-    static inline void array_init_##T(struct array_##T *array,                  \
+    static inline void array_init_##N(struct array_##N *array,                  \
                                       struct allocator* allocator) {            \
         CE_ASSERT("array_"#T, array != NULL);                                   \
         CE_ASSERT("array_"#T, allocator != NULL);                               \
@@ -55,7 +61,7 @@
         array->allocator = allocator;                                           \
     }                                                                           \
                                                                                 \
-    static inline  void array_destroy_##T(struct array_##T *a) {                \
+    static inline  void array_destroy_##N(struct array_##N *a) {                \
         CE_ASSERT("array_"#T, a != NULL);                                       \
         CE_ASSERT("array_"#T, a->allocator != NULL);                            \
         allocator_deallocate(a->allocator, a->data);                            \
@@ -65,10 +71,10 @@
         a->allocator = NULL;                                                    \
     }                                                                           \
                                                                                 \
-    static inline  void array_resize_##T(struct array_##T *a, size_t newsize);  \
-    static inline  void array_grow_##T(struct array_##T *a, size_t mincapacity);\
+    static inline  void array_resize_##N(struct array_##N *a, size_t newsize);  \
+    static inline  void array_grow_##N(struct array_##N *a, size_t mincapacity);\
                                                                                 \
-    static inline  void array_setcapacity_##T(struct array_##T *a,              \
+    static inline  void array_setcapacity_##N(struct array_##N *a,              \
                                               size_t  newcapacity) {            \
         CE_ASSERT("array_"#T, a != NULL);                                       \
         CE_ASSERT("array_"#T, a->allocator != NULL);                            \
@@ -78,7 +84,7 @@
         }                                                                       \
                                                                                 \
         if (newcapacity < a->size) {                                            \
-            array_resize_##T(a, newcapacity);                                   \
+            array_resize_##N(a, newcapacity);                                   \
         }                                                                       \
                                                                                 \
         T* newdata = 0;                                                         \
@@ -95,7 +101,7 @@
         a->capacity = newcapacity;                                              \
     }                                                                           \
                                                                                 \
-    static inline  void array_grow_##T(struct array_##T *a,                     \
+    static inline  void array_grow_##N(struct array_##N *a,                     \
                                        size_t mincapacity) {                    \
         CE_ASSERT("array_"#T, a != NULL);                                       \
         size_t newcapacity = a->capacity * 2 + 8;                               \
@@ -104,47 +110,47 @@
             newcapacity = mincapacity;                                          \
         }                                                                       \
                                                                                 \
-        array_setcapacity_##T(a, newcapacity);                                  \
+        array_setcapacity_##N(a, newcapacity);                                  \
     }                                                                           \
                                                                                 \
-    static inline  void array_resize_##T(struct array_##T *a, size_t newsize) { \
+    static inline  void array_resize_##N(struct array_##N *a, size_t newsize) { \
         CE_ASSERT("array_"#T, a != NULL);                                       \
         if (newsize > a->capacity) {                                            \
-            array_grow_##T(a, newsize);                                         \
+            array_grow_##N(a, newsize);                                         \
         }                                                                       \
                                                                                 \
         a->size = newsize;                                                      \
     }                                                                           \
                                                                                 \
-    static inline  void array_reserve_##T(struct array_##T *a,                  \
+    static inline  void array_reserve_##N(struct array_##N *a,                  \
                                           size_t new_capacity) {                \
         CE_ASSERT("array_"#T, a != NULL);                                       \
         if (new_capacity > a->capacity) {                                       \
-            array_setcapacity_##T(a, new_capacity);                             \
+            array_setcapacity_##N(a, new_capacity);                             \
         }                                                                       \
     }                                                                           \
                                                                                 \
-    static inline  void array_push_back_##T(struct array_##T *a, T item) {      \
+    static inline  void array_push_back_##N(struct array_##N *a, T item) {      \
         CE_ASSERT("array_"#T, a != NULL);                                       \
         if (a->size + 1 > a->capacity) {                                        \
-            array_grow_##T(a, 0);                                               \
+            array_grow_##N(a, 0);                                               \
         }                                                                       \
         a->data[a->size++] = item;                                              \
     }                                                                           \
                                                                                 \
-    static inline  void array_push_##T(struct array_##T *a,                     \
+    static inline  void array_push_##N(struct array_##N *a,                     \
                                        T* items,                                \
                                        size_t count) {                          \
         CE_ASSERT("array_"#T, a != NULL);                                       \
         if (a->capacity <= a->size + count) {                                   \
-            array_grow_##T(a, a->size + count);                                 \
+            array_grow_##N(a, a->size + count);                                 \
         }                                                                       \
                                                                                 \
         memory_copy(&a->data[a->size], items, sizeof(T) * count);               \
         a->size += count;                                                       \
     }                                                                           \
                                                                                 \
-    static inline  void array_pop_back_##T(struct array_##T *a) {               \
+    static inline  void array_pop_back_##N(struct array_##N *a) {               \
         CE_ASSERT("array_"#T, a != NULL);                                       \
         CE_ASSERT("array_"#T, a->size != 0);                                    \
                                                                                 \
@@ -156,9 +162,9 @@
 // Predefined array for prim types
 //==============================================================================
 
-typedef char *pchar;
+ARRAY_PROTOTYPE_N(void*, void)
 
-ARRAY_PROTOTYPE(pchar)
+ARRAY_PROTOTYPE_N(char*, pchar)
 
 ARRAY_PROTOTYPE(char)
 
