41d06b0a58d46616febdc2d0cf8ac216e638c9d4
Ondra Voves <o.voves@gmail.com>
2016-09-12 23:00:14 +0200

    prepare fpscamera

--- core/core/fpscamera.lua
+++ core/core/fpscamera.lua
@@ -1,12 +1,17 @@
 require 'core/class'
 
+local Transform = cetech.Transform
+local Camera = cetech.Camera
+local Mat4f = cetech.Mat4f
+local Quatf = cetech.Quatf
+
 FPSCamera = class(FPSCamera)
 
 function FPSCamera:init(world, camera_unit, fly_mode)
     self.world = world
     self.unit = camera_unit
-    self.camera = Camera.GetCamera(world, camera_unit)
-    self.transform = Transform.GetTransform(world, camera_unit)
+    self.camera = Camera.get(world, camera_unit)
+    self.transform = Transform.get(world, camera_unit)
 
     self.fly_mode = fly_mode or false
 end
@@ -16,20 +21,20 @@ function FPSCamera:setFlyMode(enable)
 end
 
 function FPSCamera:update(dt, dx, dy, updown, leftright)
-    local pos = Transform.GetPosition(self.world, self.transform)
-    local rot = Transform.GetRotation(self.world, self.transform)
+    local pos = Transform.get_position(self.world, self.transform)
+    local rot = Transform.get_rotation(self.world, self.transform)
 
     --Log.Info("pos", "{0} {1} {2}", pos.X, pos.Y, pos.Z)
 
-    local m_world = Transform.GetWorldMatrix(self.world, self.transform)
+    local m_world = Transform.get_world_matrix(self.world, self.transform)
     local z_dir = -Mat4f.Z(m_world)
     local x_dir = Mat4f.X(m_world)
 
     -- Rotation
-    local rotation_around_world_up = Quatf.FromAxisAngle(Vec3f.UnitY, dx * 1.0)
-    local rotation_around_camera_right = Quatf.FromAxisAngle(x_dir, -dy * 1.0)
+    local rotation_around_world_up = Quatf.from_axis_angle(Vec3f.UnitY, dx * 1.0)
+    local rotation_around_camera_right = Quatf.from_axis_angle(x_dir, -dy * 1.0)
     local rotation = rotation_around_world_up * rotation_around_camera_right
-    Transform.SetRotation(self.world, self.transform, rotation*rot)
+    Transform.set_rotation(self.world, self.transform, rotation * rot)
 
     -- Position
     if not self.fly_mode then
@@ -41,6 +46,5 @@ function FPSCamera:update(dt, dx, dy, updown, leftright)
 
     --Log.Info("z_dir", "{0} {1} {2}", z_dir.X, z_dir.Y, z_dir.Z)
 
-
-    Transform.SetPosition(self.world, self.transform, pos)
+    Transform.set_position(self.world, self.transform, pos)
 end
