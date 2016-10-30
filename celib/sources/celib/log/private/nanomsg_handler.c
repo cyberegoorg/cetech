3592536dfd1b31ec933ee5a2de49c4e109af7636
Ondra Voves <o.voves@gmail.com>
2016-10-30 18:43:28 +0100

    update mpack

--- celib/sources/celib/log/private/nanomsg_handler.c
+++ celib/sources/celib/log/private/nanomsg_handler.c
@@ -57,10 +57,9 @@ void nano_log_handler(enum log_level level,
 
     mpack_finish_map(&writer);
 
-    CE_ASSERT(LOG_WHERE, mpack_writer_destroy(&writer) == mpack_ok);
-
     bytes = nn_send(socket, data, size, 0);
     CE_ASSERT(LOG_WHERE, (size_t) bytes == size);
 
+    CE_ASSERT(LOG_WHERE, mpack_writer_destroy(&writer) == mpack_ok);
     free(data);
 }
