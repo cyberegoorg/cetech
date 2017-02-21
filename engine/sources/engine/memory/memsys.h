5d4f4273be97704be5c0e88735158cffd2001eff
Ondra Voves <ondra.voves@cyberego.org>
2017-02-21 20:43:02 +0100

    ENGINE-143 - Memory api doc

--- engine/sources/engine/memory/memsys.h
+++ engine/sources/engine/memory/memsys.h
@@ -1,23 +1,40 @@
+//! \defgroup Memory
+//! Memory system
+//! \{
+
 #ifndef CELIB_MEMSYS_H
 #define CELIB_MEMSYS_H
 
 #include "celib/memory/types.h"
 
+//! Init memory system
+//! \param scratch_buffer_size Main scratch buffer size
 void memsys_init(int scratch_buffer_size);
 
+//! Shutdown memory system
 void memsys_shutdown();
 
+//! Main allcator
+//! \return Main alocator
 struct cel_allocator *_memsys_main_allocator();
 
+//! Main scratch allocator
+//! \return Main scratch alocator
 struct cel_allocator *_memsys_main_scratch_allocator();
 
-#include "celib/memory/types.h"
 
+//! Memory system API V1
 struct MemSysApiV1 {
+    //! Main allcator
+    //! \return Main alocator
     struct cel_allocator *(*main_allocator)();
 
+    //! Main scratch allocator
+    //! \return Main scratch alocator
     struct cel_allocator *(*main_scratch_allocator)();
 };
 
 
 #endif //CELIB_MEMSYS_H
+
+//! \}
