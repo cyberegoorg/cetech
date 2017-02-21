00afb39e1d4bb94908647ad5b8314f3b75061378
Ondra Voves <ondra.voves@cyberego.org>
2017-02-21 20:44:25 +0100

    ENGINE-144 - plugin system api

--- engine/sources/engine/plugin/plugin_api.h
+++ engine/sources/engine/plugin/plugin_api.h
@@ -1,3 +1,5 @@
+//! \addtogroup Plugin
+//! \{
 #ifndef CETECH_PLUGIN_API_H
 #define CETECH_PLUGIN_API_H
 
@@ -6,9 +8,11 @@
 
 enum {
     PLUGIN_EXPORT_API_ID = 1,
+    APPLICATION_API_ID,
     KEYBOARD_API_ID,
     MOUSE_API_ID,
     GAMEPAD_API_ID,
+    MACHINE_API_ID,
     CONFIG_API_ID,
     TASK_API_ID,
     CONSOLE_SERVER_API_ID,
@@ -33,21 +37,36 @@ enum {
 typedef void *(*get_api_fce_t)(int api,
                                int version);
 
+//! Plugin expot api struct V1
 struct plugin_api_v0 {
+
+    //! Init cvars
+    //! \param Init cvars
     void (*init_cvar       )(struct ConfigApiV1);
 
+    //! Init
+    //! \param get_api_fce_t Get engine api
     void (*init            )(get_api_fce_t);
 
+    //! Shutdown
     void (*shutdown        )(void);
 
-    void *(*reload_begin    )(get_api_fce_t);
+    //! Reload begin
+    //! \param get_api_fce_t Get engine api
+    void *(*reload_begin   )(get_api_fce_t);
 
+    //! Reload end
+    //! \param get_api_fce_t Get engine api
     void (*reload_end      )(get_api_fce_t,
                              void *);
 
+    //! Call update
     void (*update          )();
 
+    //! After update
+    //! \param dt Deltatime
     void (*after_update    )(float dt);
 };
 
 #endif //CETECH_PLUGIN_API_H
+//! \}
