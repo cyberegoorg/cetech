356a9a2cf269b781035ebab297b6e97554b1d9b2
Ondra Voves <ondra.voves@cyberego.org>
2017-02-21 20:40:12 +0100

    ENGINE-140 - EntCom api doc

--- engine/sources/engine/entcom/entcom.h
+++ engine/sources/engine/entcom/entcom.h
@@ -1,3 +1,6 @@
+//! \defgroup EntityComponent
+//! Entity-Component system
+//! \{
 #ifndef CETECH_ENTITY_MANAGER_H
 #define CETECH_ENTITY_MANAGER_H
 
@@ -10,6 +13,8 @@
 #include <celib/handler/handlerid.h>
 #include <engine/world/world.h>
 
+
+//! Entity typedef
 typedef struct {
     union {
         handler_t h;
@@ -20,13 +25,27 @@ typedef struct {
 ARRAY_PROTOTYPE(entity_t);
 MAP_PROTOTYPE(entity_t);
 
+//! Component compiler
+//! \param body Component body yaml
+//! \param data Compiled compoent data
 typedef int (*component_compiler_t)(yaml_node_t body,
                                     ARRAY_T(u8) *data);
 
+//! Component destoryer
+//! \param world World where is component
+//! \param ents Destroy component for this entity
+//! \param ent_count Entity count
 typedef void (*component_destroyer_t)(world_t world,
                                       entity_t *ents,
                                       size_t ent_count);
 
+//! Component spawner
+//! \param world World where component live
+//! \param ents Create component for this entities
+//! \param cent Entity map
+//! \param ents_parent Parent map
+//! \param ent_count Entity count
+//! \param data Component data
 typedef void (*component_spawner_t)(world_t world,
                                     entity_t *ents,
                                     u32 *cent,
@@ -34,35 +53,67 @@ typedef void (*component_spawner_t)(world_t world,
                                     size_t ent_count,
                                     void *data);
 
+//! Component callbacks
 static struct component_clb {
-    component_spawner_t spawner;
-    component_destroyer_t destroyer;
-    world_on_created_t on_world_create;
-    world_on_destroy_t on_world_destroy;
-    world_on_update_t on_world_update;
+    component_spawner_t spawner;         //!< Spawner
+    component_destroyer_t destroyer;     //!< Destroyer
+    world_on_created_t on_world_create;  //!< On world create
+    world_on_destroy_t on_world_destroy; //!< On world destroy
+    world_on_update_t on_world_update;   //!< On world update
 } component_clb_null = {0};
 
 
+//! Entity component system API V1
 struct EntComSystemApiV1 {
+    //! Create new entity
+    //! \return New entity
     entity_t (*entity_manager_create)();
 
+    //! Destoy entity
+    //! \param entity Entity to destroy
     void (*entity_manager_destroy)(entity_t entity);
 
+    //! Is entity alive?
+    //! \param entity Entity
+    //! \return 1 if entity is alive else 0
     int (*entity_manager_alive)(entity_t entity);
 
+    //! Register component compiler
+    //! \param type Component type
+    //! \param compiler Compiler fce
+    //! \param spawn_order Spawn order number
     void (*component_register_compiler)(stringid64_t type,
                                         component_compiler_t compiler,
                                         u32 spawn_order);
 
+    //! Compile component
+    //! \param type Component type
+    //! \param body Component yaml body
+    //! \param data Compiled data
+    //! \return 1 if compile is ok else 0
     int (*component_compile)(stringid64_t type,
                              yaml_node_t body,
                              ARRAY_T(u8) *data);
 
+    //! Get component spawn order
+    //! \param type Component type
+    //! \return Spawn order
     u32 (*component_get_spawn_order)(stringid64_t type);
 
+    //! Register new component type
+    //! \param type Component type
+    //! \param clb Callbacks
     void (*component_register_type)(stringid64_t type,
                                     struct component_clb clb);
 
+    //! Spawn components
+    //! \param world World where component live
+    //! \param type Component type
+    //! \param ents Create component for this entities
+    //! \param cent Entity map
+    //! \param ents_parent Parent map
+    //! \param ent_count Entity count
+    //! \param data Component data
     void (*component_spawn)(world_t world,
                             stringid64_t type,
                             entity_t *ent_ids,
@@ -71,6 +122,10 @@ struct EntComSystemApiV1 {
                             u32 ent_count,
                             void *data);
 
+    //! Destroy components
+    //! \param world World where component live
+    //! \param ent Destroy component for this entities
+    //! \param count Entities count
     void (*component_destroy)(world_t world,
                               entity_t *ent,
                               u32 count);
@@ -78,3 +133,4 @@ struct EntComSystemApiV1 {
 
 
 #endif //CETECH_ENTITY_MANAGER_H
+//! \}
