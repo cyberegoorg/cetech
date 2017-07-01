20c6dc1c02dbfa3189b18681fb1526fb03458093
Ondra Voves <ondra.voves@cyberego.org>
2017-07-01 11:03:33 +0200

    CEBASE-18 - Refactor blobinator - cpp template

--- scripts/templates/blob.h
+++ scripts/templates/blob.h
@@ -1,32 +1,54 @@
 #ifndef {{ guard_prefix }}_{{ name|upper }}_BLOB_H
 #define {{ guard_prefix }}_{{ name|upper }}_BLOB_H
 
+#include <stdint.h>
 {% for include in includes %}
 #include {{ include }}
 {% endfor %}
 
-typedef struct {{name}}_blob {
-    {% for k, v in elements.items() %}
-        {% if v.is_array %}
-    // {{ v.full_type }} {{ v.name }}[{{ v.count }}];
-        {% else %}
-    {{ v.type }} {{ v.name }};
-        {% endif %}
-    {% endfor %}
-} {{name}}_blob_t;
+namespace {{name}}_blob {
+
+    typedef struct {
+        uint32_t blob_version;
+
+        {% for k, v in elements.items() %}
+            {% if v.is_array %}
+        // {{ v.full_type }} {{ v.name }}[{{ v.count }}];
+            {% else %}
+        {{ v.type }} {{ v.name }};
+            {% endif %}
+        {% endfor %}
+    } blob_t;
+
+    inline const blob_t* get(void* data) {
+        return (blob_t*)(data);
+    }
 
 {% for k, v in elements.items() %}
     {% if v.is_array %}
         {% if v.is_first_array %}
-#define {{ name }}_blob_{{ v.name }}(r) (({{v.type}}*) ((r) + 1))
+    inline {{v.type}}* {{ v.name }}(const blob_t* blob) {
+        return ({{v.type}}*)((blob) + 1);
+    }
         {% else %}
             {% if v.prev_item.type_count != None %}
-#define {{ name }}_blob_{{ v.name }}(r) (({{v.type}}*) ({{ name }}_blob_{{ v.prev_item.name }}(r) + ((r)->{{ v.prev_item.count}}*{{v.prev_item.type_count}})))
+    inline {{v.type}}* {{ v.name }}(const blob_t* blob) {
+        return (({{v.type}}*) ({{ v.prev_item.name }}(blob) + (blob->{{ v.prev_item.count}}*{{v.prev_item.type_count}})));
+    }
             {% else %}
-#define {{ name }}_blob_{{ v.name }}(r) (({{v.type}}*) ({{ name }}_blob_{{ v.prev_item.name }}(r) + ((r)->{{ v.prev_item.count}})))
+    inline {{v.type}}* {{ v.name }}(const blob_t* blob) {
+        return (({{v.type}}*) ({{ v.prev_item.name }}(blob) + (blob->{{ v.prev_item.count}})));
+    }
             {% endif %}
         {% endif %}
+    {% else %}
+    inline {{v.type}} {{ v.name }}(const blob_t* blob) {
+        return blob->{{ v.name}};
+    }
     {% endif %}
+
 {% endfor %}
+
+}
 
 #endif // {{ guard_prefix }}_{{ name|upper }}_BLOB_H
