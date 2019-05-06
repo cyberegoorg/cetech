78a3f8ecaa212f707b4860ed692fb72bec6e9a56
Ondra Voves <ondra.voves@cyberego.org>
2019-05-06 20:48:05 +0200

    remove assimp, initial use of cgltf

---
+++ src/cetech/scene/private/gltf_compiler.inl
@@ -0,0 +1,228 @@
+#define CGLTF_IMPLEMENTATION
+#include "include/cgltf/cgltf.h"
+
+static bgfx_attrib_t _gltf_attr_to_bgfx(cgltf_attribute_type attr) {
+    switch (attr) {
+        case cgltf_attribute_type_invalid:
+            break;
+
+        case cgltf_attribute_type_position:
+            return BGFX_ATTRIB_POSITION;
+
+        case cgltf_attribute_type_normal:
+            return BGFX_ATTRIB_NORMAL;
+
+        case cgltf_attribute_type_tangent:
+            return BGFX_ATTRIB_TANGENT;
+
+        case cgltf_attribute_type_texcoord:
+            return BGFX_ATTRIB_TEXCOORD0;
+
+        case cgltf_attribute_type_color:
+            return BGFX_ATTRIB_COLOR0;
+
+        case cgltf_attribute_type_joints:
+            break;
+
+        case cgltf_attribute_type_weights:
+            return BGFX_ATTRIB_WEIGHT;
+    }
+
+    return BGFX_ATTRIB_COUNT;
+}
+
+static bgfx_attrib_type_t _gltf_acc_comp_type_to_bgfx(cgltf_component_type type) {
+    switch (type) {
+        case cgltf_component_type_invalid:
+            break;
+
+        case cgltf_component_type_r_8:
+            break;
+
+        case cgltf_component_type_r_8u:
+            return BGFX_ATTRIB_TYPE_UINT8;
+            break;
+
+        case cgltf_component_type_r_16:
+            return BGFX_ATTRIB_TYPE_INT16;
+            break;
+
+        case cgltf_component_type_r_16u:
+            break;
+
+        case cgltf_component_type_r_32u:
+            break;
+
+        case cgltf_component_type_r_32f:
+            return BGFX_ATTRIB_TYPE_FLOAT;
+            break;
+    }
+
+    return BGFX_ATTRIB_TYPE_COUNT;
+}
+
+static int _comp_size(bgfx_attrib_type_t type) {
+    switch (type) {
+        case BGFX_ATTRIB_TYPE_UINT8:
+            return sizeof(uint8_t);
+            break;
+        case BGFX_ATTRIB_TYPE_UINT10:
+            break;
+        case BGFX_ATTRIB_TYPE_INT16:
+            return sizeof(uint16_t);
+            break;
+        case BGFX_ATTRIB_TYPE_HALF:
+            break;
+        case BGFX_ATTRIB_TYPE_FLOAT:
+            return sizeof(float);
+            break;
+        case BGFX_ATTRIB_TYPE_COUNT:
+            break;
+    }
+
+    return 0;
+}
+
+static int _gltf_comp_num(cgltf_type type) {
+    switch (type) {
+        default:
+        case cgltf_type_invalid:
+            return  0;
+
+        case cgltf_type_scalar:
+            return 1;
+
+        case cgltf_type_vec2:
+            return 2;
+
+        case cgltf_type_vec3:
+            return 3;
+
+        case cgltf_type_vec4:
+            return 4;
+
+        case cgltf_type_mat2:
+            return 2 * 2;
+
+        case cgltf_type_mat3:
+            return 3 * 3;
+
+        case cgltf_type_mat4:
+            return 4 * 4;
+    }
+}
+
+static int _compile_gtlf(ce_cdb_t0 db,
+                         uint64_t k,
+                         struct compile_output *output) {
+    const ce_cdb_obj_o0 *reader = ce_cdb_a0->read(db, k);
+    uint64_t import_obj = ce_cdb_a0->read_subobject(reader, SCENE_IMPORT_PROP, 0);
+
+    ct_scene_import_obj_t0 io = {};
+    ce_cdb_a0->read_to(db, import_obj, &io, sizeof(io));
+
+    const ce_cdb_obj_o0 *c_reader = ce_cdb_a0->read(ce_cdb_a0->db(), ce_config_a0->obj());
+    const char *source_dir = ce_cdb_a0->read_str(c_reader, CONFIG_SRC, "");
+
+    char *input_path = NULL;
+    ce_os_path_a0->join(&input_path, _G.allocator, 2, source_dir, io.input);
+
+    cgltf_options options = {};
+    cgltf_data *data = NULL;
+    cgltf_result result = cgltf_parse_file(&options, input_path, &data);
+    if (result != cgltf_result_success) {
+        return 0;
+    }
+
+    result = cgltf_load_buffers(&options, data, input_path);
+    if (result != cgltf_result_success) {
+        return 0;
+    }
+
+    for (int m = 0; m < data->meshes_count; ++m) {
+        cgltf_mesh *mesh = &data->meshes[m];
+
+        size_t name_len = strlen(mesh->name);
+
+        char tmp_buffer[1024] = {};
+        char tmp_buffer2[1024] = {};
+        uint32_t unique = 0;
+
+        if (name_len == 0) {
+            snprintf(tmp_buffer, CE_ARRAY_LEN(tmp_buffer), "geom_%d", m);
+        } else {
+            memcpy(tmp_buffer, mesh->name, name_len);
+        }
+
+        uint64_t name_id = ce_id_a0->id64(tmp_buffer);
+        for (uint32_t i = 0; k < ce_array_size(output->geom_name); ++i) {
+            if (name_id == output->geom_name[k]) {
+                snprintf(tmp_buffer2, CE_ARRAY_LEN(tmp_buffer2), "%s%d", tmp_buffer, ++unique);
+                snprintf(tmp_buffer, CE_ARRAY_LEN(tmp_buffer), "%s", tmp_buffer2);
+                break;
+            }
+        }
+
+        char tmp_name[128] = {};
+        strncpy(tmp_name, tmp_buffer, 127);
+        ce_array_push_n(output->geom_str, &tmp_name, 1, _G.allocator);
+        ce_array_push(output->geom_name, ce_id_a0->id64(tmp_buffer), _G.allocator);
+        ce_array_push(output->ib_offset, ce_array_size(output->ib), _G.allocator);
+        ce_array_push(output->vb_offset, ce_array_size(output->vb), _G.allocator);
+
+        bgfx_vertex_decl_t vertex_decl;
+        ct_gfx_a0->bgfx_vertex_decl_begin(&vertex_decl, BGFX_RENDERER_TYPE_COUNT);
+
+        uint32_t v_size = 0;
+        uint32_t vertex_n = 0;
+        cgltf_primitive *prim = &mesh->primitives[0];
+        for (int l = 0; l < prim->attributes_count; ++l) {
+            cgltf_attribute *attr = &prim->attributes[l];
+
+            vertex_n = attr->data->count;
+
+            bgfx_attrib_t bgfx_attr = _gltf_attr_to_bgfx(attr->type);
+            bgfx_attrib_type_t bgfx_attr_t = \
+                _gltf_acc_comp_type_to_bgfx(attr->data->component_type);
+
+            int num = _gltf_comp_num(attr->data->type);
+            bool normalized = bgfx_attr == BGFX_ATTRIB_NORMAL;
+
+            ct_gfx_a0->bgfx_vertex_decl_add(&vertex_decl,
+                                            bgfx_attr, num,
+                                            bgfx_attr_t, normalized, 0);
+
+            v_size += _comp_size(bgfx_attr_t) * num;
+        }
+
+        ct_gfx_a0->bgfx_vertex_decl_end(&vertex_decl);
+
+        uint32_t indices_n = prim->indices->count;
+
+        ce_array_push(output->vb_decl, vertex_decl, _G.allocator);
+        ce_array_push(output->vb_size, v_size * vertex_n, _G.allocator);
+        ce_array_push(output->ib_size, indices_n, _G.allocator);
+
+        for (int j = 0; j < vertex_n; ++j) {
+            for (int l = 0; l < prim->attributes_count; ++l) {
+                cgltf_attribute *attr = &prim->attributes[l];
+
+                uint32_t c_n = cgltf_num_components(attr->data->type);
+
+                float f[c_n];
+                cgltf_accessor_read_float(attr->data, j, f, c_n);
+
+                ce_array_push_n(output->vb, (uint8_t *) f, sizeof(float) * c_n, _G.allocator);
+            }
+        }
+
+        for (int n = 0; n < indices_n; ++n) {
+            uint32_t idx = cgltf_accessor_read_index(prim->indices, n);
+            ce_array_push(output->ib, idx, _G.allocator);
+        }
+
+    }
+
+    cgltf_free(data);
+    return 1;
+}
--- src/cetech/scene/private/scene_compiler.cpp
+++ src/cetech/scene/private/scene_compiler.cpp
@@ -4,17 +4,18 @@
 #include <time.h>
 
 #include <celib/memory/allocator.h>
+#include <celib/log.h>
+#include <celib/os/path.h>
 #include <celib/macros.h>
 #include <celib/ydb.h>
 #include <celib/containers/array.h>
+#include <celib/containers/buffer.h>
 #include "celib/id.h"
 #include "celib/memory/memory.h"
 #include "celib/api.h"
-
 #include <celib/ydb.h>
 #include <celib/cdb.h>
 #include <celib/config.h>
-
 
 #include "cetech/machine/machine.h"
 #include "cetech/resource/resource.h"
@@ -24,15 +25,7 @@
 #include <cetech/scene/scene.h>
 #include <cetech/kernel/kernel.h>
 #include <cetech/resource/resourcedb.h>
-
-#include <include/assimp/scene.h>
-#include <include/assimp/postprocess.h>
-#include <include/assimp/cimport.h>
-#include <celib/log.h>
-#include <celib/containers/buffer.h>
 #include <cetech/resource/resource_compiler.h>
-#include <celib/os/path.h>
-
 
 #define _G scene_compiler_globals
 
@@ -58,43 +51,6 @@ struct compile_output {
     char_128 *geom_str; // TODO : SHIT
     char_128 *node_str; // TODO : SHIT
 };
-
-//static const struct {
-//    const char *name;
-//    bgfx_attrib_t attrib;
-//} _chanel_types[] = {
-//        {.name="position", .attrib=BGFX_ATTRIB_POSITION},
-//        {.name="normal", .attrib=BGFX_ATTRIB_NORMAL},
-//        {.name="tangent", .attrib=BGFX_ATTRIB_TANGENT},
-//        {.name="bitangent", .attrib=BGFX_ATTRIB_BITANGENT},
-//        {.name="color0", .attrib=BGFX_ATTRIB_COLOR0},
-//        {.name="color1", .attrib=BGFX_ATTRIB_COLOR1},
-//        {.name="color2", .attrib=BGFX_ATTRIB_COLOR2},
-//        {.name="color3", .attrib=BGFX_ATTRIB_COLOR3},
-//        {.name="indices", .attrib=BGFX_ATTRIB_INDICES},
-//        {.name="weight", .attrib=BGFX_ATTRIB_INDICES},
-//        {.name="texcoord0", .attrib=BGFX_ATTRIB_TEXCOORD0},
-//        {.name="texcoord1", .attrib=BGFX_ATTRIB_TEXCOORD1},
-//        {.name="texcoord2", .attrib=BGFX_ATTRIB_TEXCOORD2},
-//        {.name="texcoord3", .attrib=BGFX_ATTRIB_TEXCOORD3},
-//        {.name="texcoord4", .attrib=BGFX_ATTRIB_TEXCOORD4},
-//        {.name="texcoord5", .attrib=BGFX_ATTRIB_TEXCOORD5},
-//        {.name="texcoord6", .attrib=BGFX_ATTRIB_TEXCOORD6},
-//        {.name="texcoord7", .attrib=BGFX_ATTRIB_TEXCOORD7},
-//};
-//
-//static const struct {
-//    const char *name;
-//    size_t size;
-//    bgfx_attrib_type_t attrib_type;
-//} _attrin_tbl[] = {
-//        {.name="f32", .size=sizeof(float), .attrib_type=BGFX_ATTRIB_TYPE_FLOAT},
-//        {.name="int16_t", .size=sizeof(int16_t), .attrib_type=BGFX_ATTRIB_TYPE_INT16},
-//        {.name="uint8_t", .size=sizeof(uint8_t), .attrib_type=BGFX_ATTRIB_TYPE_UINT8},
-////        {.name="float16_t", .size=sizeof(float16_t), .attrib_type=BGFX_ATTRIB_TYPE_HALF},
-//        // TODO: {.name="u10", .size=sizeof(u10), .attrib_type=BGFX_ATTRIB_TYPE_UINT10},
-//};
-//
 
 struct compile_output *_crete_compile_output() {
     compile_output *output =
@@ -124,163 +80,7 @@ static void _destroy_compile_output(compile_output *output) {
     CE_FREE(_G.allocator, output);
 }
 
-static void _compile_assimp_node(aiNode *root,
-                                 uint32_t parent,
-                                 struct compile_output *output) {
-
-    uint64_t name = ce_id_a0->id64(root->mName.data);
-
-    char tmp_name[128] = {};
-    strncpy(tmp_name, root->mName.data, 127);
-    ce_array_push_n(output->node_str, &tmp_name, 1, _G.allocator);
-
-    uint32_t idx = ce_array_size(output->node_name);
-
-    ce_array_push(output->node_name, name, _G.allocator);
-    ce_array_push(output->node_parent, parent, _G.allocator);
-    ce_array_push_n(output->node_pose, &root->mTransformation.a1, 16, _G.allocator);
-
-    for (uint32_t i = 0; i < root->mNumChildren; ++i) {
-        _compile_assimp_node(root->mChildren[i], idx, output);
-    }
-
-    for (uint32_t i = 0; i < root->mNumMeshes; ++i) {
-        ce_array_push(output->geom_node, name, _G.allocator);
-    }
-}
-
-static int _compile_assimp(ce_cdb_t0 db,
-                           uint64_t k,
-                           struct compile_output *output) {
-    const ce_cdb_obj_o0 *reader = ce_cdb_a0->read(db, k);
-
-    uint64_t import_obj = ce_cdb_a0->read_subobject(reader, SCENE_IMPORT_PROP, 0);
-
-    ct_scene_import_obj_t0 io = {};
-    ce_cdb_a0->read_to(db, import_obj, &io, sizeof(io));
-
-    const ce_cdb_obj_o0 *c_reader = ce_cdb_a0->read(ce_cdb_a0->db(), ce_config_a0->obj());
-    const char *source_dir = ce_cdb_a0->read_str(c_reader, CONFIG_SRC, "");
-
-    char *input_path = NULL;
-    ce_os_path_a0->join(&input_path, _G.allocator, 2, source_dir, io.input);
-
-    uint32_t postprocess_flag = aiProcessPreset_TargetRealtime_MaxQuality
-                                | aiProcess_ConvertToLeftHanded;
-
-    if (io.flip_uvs) {
-        postprocess_flag |= aiProcess_FlipUVs;
-    }
-
-    const struct aiScene *scene = aiImportFile(input_path, postprocess_flag);
-
-    if (!scene) {
-        ce_log_a0->error("scene_compiler", "Could not import %s", input_path);
-        return 0;
-    }
-
-    char tmp_buffer[1024] = {};
-    char tmp_buffer2[1024] = {};
-    uint32_t unique = 0;
-    for (uint32_t i = 0; i < scene->mNumMeshes; ++i) {
-        struct aiMesh *mesh = scene->mMeshes[i];
-
-        if (mesh->mName.length == 0) {
-            snprintf(tmp_buffer, CE_ARRAY_LEN(tmp_buffer), "geom_%d", i);
-        } else {
-            memcpy(tmp_buffer, mesh->mName.data, mesh->mName.length);
-        }
-
-        uint64_t name_id = ce_id_a0->id64(tmp_buffer);
-        for (uint32_t k = 0; k < ce_array_size(output->geom_name); ++k) {
-            if (name_id == output->geom_name[k]) {
-                snprintf(tmp_buffer2, CE_ARRAY_LEN(tmp_buffer2), "%s%d",
-                         tmp_buffer, ++unique);
-                snprintf(tmp_buffer, CE_ARRAY_LEN(tmp_buffer), "%s",
-                         tmp_buffer2);
-                break;
-            }
-        }
-
-
-        char tmp_name[128] = {};
-        strncpy(tmp_name, tmp_buffer, 127);
-        ce_array_push_n(output->geom_str, &tmp_name, 1, _G.allocator);
-
-        ce_array_push(output->geom_name, ce_id_a0->id64(tmp_buffer),
-                      _G.allocator);
-        ce_array_push(output->ib_offset, ce_array_size(output->ib),
-                      _G.allocator);
-        ce_array_push(output->vb_offset, ce_array_size(output->vb),
-                      _G.allocator);
-        ce_array_push(output->ib_size, mesh->mNumFaces * 3, _G.allocator);
-
-        bgfx_vertex_decl_t vertex_decl;
-        ct_gfx_a0->bgfx_vertex_decl_begin(&vertex_decl,
-                                          BGFX_RENDERER_TYPE_COUNT);
-
-        uint32_t v_size = 0;
-        if (mesh->mVertices != NULL) {
-            ct_gfx_a0->bgfx_vertex_decl_add(&vertex_decl,
-                                            BGFX_ATTRIB_POSITION, 3,
-                                            BGFX_ATTRIB_TYPE_FLOAT, 0, 0);
-            v_size += 3 * sizeof(float);
-        }
-
-        if (mesh->mNormals != NULL) {
-            ct_gfx_a0->bgfx_vertex_decl_add(&vertex_decl,
-                                            BGFX_ATTRIB_NORMAL, 3,
-                                            BGFX_ATTRIB_TYPE_FLOAT, true,
-                                            0);
-            v_size += 3 * sizeof(float);
-        }
-
-        if (mesh->mTextureCoords[0] != NULL) {
-            ct_gfx_a0->bgfx_vertex_decl_add(&vertex_decl,
-                                            BGFX_ATTRIB_TEXCOORD0, 2,
-                                            BGFX_ATTRIB_TYPE_FLOAT, 0, 0);
-
-            v_size += 2 * sizeof(float);
-        }
-        ct_gfx_a0->bgfx_vertex_decl_end(&vertex_decl);
-
-        ce_array_push(output->vb_decl, vertex_decl, _G.allocator);
-        ce_array_push(output->vb_size, v_size * mesh->mNumVertices,
-                      _G.allocator);
-
-        for (uint32_t j = 0; j < mesh->mNumVertices; ++j) {
-            if (mesh->mVertices != NULL) {
-                ce_array_push_n(output->vb,
-                                (uint8_t *) &mesh->mVertices[j],
-                                sizeof(float) * 3, _G.allocator);
-            }
-
-            if (mesh->mNormals != NULL) {
-                ce_array_push_n(output->vb,
-                                (uint8_t *) &mesh->mNormals[j],
-                                sizeof(float) * 3, _G.allocator);
-            }
-
-            if (mesh->mTextureCoords[0] != NULL) {
-                ce_array_push_n(output->vb,
-                                (uint8_t *) &mesh->mTextureCoords[0][j],
-                                sizeof(float) * 2, _G.allocator);
-            }
-        }
-
-        for (uint32_t j = 0; j < mesh->mNumFaces; ++j) {
-            ce_array_push(output->ib, mesh->mFaces[j].mIndices[0],
-                          _G.allocator);
-            ce_array_push(output->ib, mesh->mFaces[j].mIndices[1],
-                          _G.allocator);
-            ce_array_push(output->ib, mesh->mFaces[j].mIndices[2],
-                          _G.allocator);
-        }
-    }
-
-    _compile_assimp_node(scene->mRootNode, UINT32_MAX, output);
-    return 1;
-}
+#include "gltf_compiler.inl"
 
 extern "C" bool scene_compiler(ce_cdb_t0 db,
                                uint64_t obj) {
@@ -291,7 +91,14 @@ extern "C" bool scene_compiler(ce_cdb_t0 db,
     const ce_cdb_obj_o0 *reader = ce_cdb_a0->read(db, obj);
 
     if (ce_cdb_a0->prop_exist(reader, SCENE_IMPORT_PROP)) {
-        ret = _compile_assimp(db, obj, output);
+        uint64_t import_obj = ce_cdb_a0->read_subobject(reader, SCENE_IMPORT_PROP, 0);
+
+        ct_scene_import_obj_t0 io = {};
+        ce_cdb_a0->read_to(db, import_obj, &io, sizeof(io));
+
+        if (strstr(io.input, ".gltf") != NULL) {
+            ret = _compile_gtlf(db, obj, output);
+        }
     }
 
     if (!ret) {
